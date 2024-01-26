a2e0f48642ef0c45a3d734be67062ec86679f4c5
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2024-01-26 11:41:00 +0000

    fix: import casing error in test

--- apps/official-journal-api/src/app/journal.controller.spec.ts
+++ apps/official-journal-api/src/app/journal.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
 import { LOGGER_PROVIDER } from '@dmr.is/logging'
-import { JournalController } from './Journal.controller'
+import { JournalController } from './journal.controller'
 import { IJournalService } from './journal.service.interface'
 import { MockJournalService } from './journal.service.mock'
 
