573eeb76e9e618b4b3097fc3ba5fd915f5ba84ff
Jón Bjarni Ólafsson <92530555+jonbjarnio@users.noreply.github.com>
2024-03-12 14:03:51 +0000

    fix(api): Statistics openapi generation (#21)
    * Openapi generation fix for statistics module
    
    * Fixed the tests... again, removed uneccary tests.

--- apps/official-journal-api/src/app/statistics/statistics.service.interface.ts
+++ apps/official-journal-api/src/app/statistics/statistics.service.interface.ts
@@ -1,16 +1,10 @@
-import { StatisticsDepartmentQuery } from '../../dto/statistics/statistics-department-query.dto'
 import { StatisticsDepartmentResponse } from '../../dto/statistics/statistics-department.dto'
 import { StatisticsOverviewResponse } from '../../dto/statistics/statistics-overview-dto'
-import { StatisticsOverviewQuery } from '../../dto/statistics/statistics-overview-query.dto'
 
 export interface IStatisticsService {
-  getDepartment(
-    params?: StatisticsDepartmentQuery,
-  ): Promise<StatisticsDepartmentResponse>
+  getDepartment(type: string): Promise<StatisticsDepartmentResponse>
 
-  getOverview(
-    params?: StatisticsOverviewQuery,
-  ): Promise<StatisticsOverviewResponse>
+  getOverview(id: string): Promise<StatisticsOverviewResponse>
 }
 
 export const IStatisticsService = Symbol('IStatisticsService')
