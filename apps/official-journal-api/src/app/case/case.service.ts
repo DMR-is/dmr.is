7730f1b86206548255901dea830d843643d4054e
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2024-03-12 15:52:14 +0000

    Adding case module with one endpoint to app

---
+++ apps/official-journal-api/src/app/case/case.service.ts
@@ -0,0 +1,15 @@
+import { Inject, Injectable, NotImplementedException } from '@nestjs/common'
+import { ICaseService } from './case.service.interface'
+import { Case } from '../../dto/case/case.dto'
+import { LOGGER_PROVIDER, Logger } from '@dmr.is/logging'
+
+@Injectable()
+export class CaseService implements ICaseService {
+  constructor(@Inject(LOGGER_PROVIDER) private readonly logger: Logger) {
+    this.logger.info('CaseService')
+  }
+  getCase(id: string): Promise<Case | null> {
+    this.logger.info(id)
+    throw new NotImplementedException()
+  }
+}
