181a71283aa88864ef8dd40196870ec439adb122
Benedikt Óskarsson <bensi94@hotmail.com>
2024-03-27 10:07:59 +0000

    ci: add admin api (#38)

--- apps/official-journal-admin-api/src/app/app.module.ts
+++ apps/official-journal-admin-api/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 import { RouterModule } from '@nestjs/core'
 import { CaseModule } from './case/case.module'
+import { HealthModule } from './health/health.module'
 import { StatisticsModule } from './statistics/statistics.module'
 
 @Module({
   imports: [
     CaseModule,
+    HealthModule,
     StatisticsModule,
     RouterModule.register([
       {
@@ -16,7 +18,11 @@ import { StatisticsModule } from './statistics/statistics.module'
         path: 'statistics',
         module: StatisticsModule,
       },
+      {
+        path: 'health',
+        module: HealthModule,
+      }
     ]),
   ],
 })
-export class AppModule {}
+export class AppModule { }
