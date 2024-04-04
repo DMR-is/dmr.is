136a270241ec05e91688a59a172342e9492fb60f
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2024-04-04 11:03:03 +0000

    case filters

---
+++ apps/official-journal-web/src/components/case-filters/messages.ts
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl'
+
+export const messages = {
+  general: defineMessages({
+    searchPlaceholder: {
+      id: 'web.components.caseFilters:searchPlaceholder',
+      defaultMessage: 'Leita eftir málsnafni',
+      description: 'Search placeholder for case filters',
+    },
+    openFilter: {
+      id: 'web.components.caseFilters:openFilter',
+      defaultMessage: 'Opna síun',
+      description: 'Open filter button label for case filters',
+    },
+  }),
+}
--- apps/official-journal-web/src/lib/messages/caseOverview.ts
+++ apps/official-journal-web/src/lib/messages/caseOverview.ts
@@ -3,34 +3,34 @@ import { defineMessages } from 'react-intl'
 export const messages = {
   tabs: defineMessages({
     submitted: {
-      id: 'web.caseOverview.tabs.submitted',
+      id: 'web.page.caseOverview:tabs.submitted',
       defaultMessage: 'Innsendingar {count}',
       description: 'Tab label for submitted cases',
     },
     inProgress: {
-      id: 'web.caseOverview.tabs.inProgress',
+      id: 'web.page.caseOverview:tabs.inProgress',
       defaultMessage: 'Grunnvinnsla {count}',
       description: 'Tab label for cases in progress',
     },
     inReview: {
-      id: 'web.caseOverview.tabs.inReview',
+      id: 'web.page.caseOverview:tabs.inReview',
       defaultMessage: 'Yfirlestur {count}',
       description: 'Tab label for cases in review',
     },
     ready: {
-      id: 'web.caseOverview.tabs.ready',
+      id: 'web.page.caseOverview:tabs.ready',
       defaultMessage: 'Tilbúið {count}',
       description: 'Tab label for cases ready',
     },
   }),
   banner: defineMessages({
     title: {
-      id: 'web.caseOverview.banner.title',
+      id: 'web.page.caseOverview:banner.title',
       defaultMessage: 'Vinnslusvæði Stjórnartíðinda',
       description: 'Title for the banner on the case overview page',
     },
     description: {
-      id: 'web.caseOverview.banner.description',
+      id: 'web.page.caseOverview:banner.description',
       defaultMessage:
         'Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis',
       description: 'Description for the banner on the case overview page',
@@ -38,12 +38,12 @@ export const messages = {
   }),
   breadcrumbs: defineMessages({
     home: {
-      id: 'web.caseOverview.breadcrumbs.home',
+      id: 'web.page.caseOverview:breadcrumbs.home',
       defaultMessage: 'Stjórnartíðindi',
       description: 'Breadcrumb for the home page',
     },
     cases: {
-      id: 'web.caseOverview.breadcrumbs.cases',
+      id: 'web.page.caseOverview:breadcrumbs.cases',
       defaultMessage: 'Ritstjórn',
       description: 'Breadcrumb for the case overview page',
     },
