e69224503f92db40dcfe98d0d4abbc1b04621821
Valur Sverrisson <valur@hugsmidjan.is>
2024-04-09 13:46:52 +0000

    fix: suppress invalid IntlProvider errors

--- apps/official-journal-web/src/layout/Layout.tsx
+++ apps/official-journal-web/src/layout/Layout.tsx
@@ -41,7 +41,22 @@ const Layout: Screen<LayoutProps> = ({
   ]
 
   return (
-    <IntlProvider locale="is" messages={icelandic}>
+    <IntlProvider
+      locale="is"
+      defaultLocale="is"
+      messages={icelandic}
+      onError={(err) => {
+        // Chrome only ships with 'en' formatters for NumberFormat and DateTimeFormat.
+        // Ignore these errors since we're not using these formatters.
+        // Bundling polyfills for 'is' significantly increases bundle size and provides no gain.
+        // See: https://app.asana.com/0/1202453499137756/1204509391926816
+        if (err.code === 'MISSING_DATA') {
+          return null
+        }
+
+        console.error('Error in IntlProvider', { exception: err })
+      }}
+    >
       <Provider>
         <NotificationContextProvider>
           <FilterContextProvider filterGroups={filterGroups}>
