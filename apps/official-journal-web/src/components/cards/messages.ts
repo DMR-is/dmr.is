8841b9bcd6c5657a8ff9741bf8f5daf3e2febc7d
Valur Sverrisson <valur@hugsmidjan.is>
2024-04-19 13:12:22 +0000

    fix: add CaseCard link

---
+++ apps/official-journal-web/src/components/cards/messages.ts
@@ -0,0 +1,11 @@
+import { defineMessages } from 'react-intl'
+
+export const messages = {
+  general: defineMessages({
+    moreLink: {
+      id: 'web.components.cards:moreLink',
+      defaultMessage: 'Sjá nánar',
+      description: '',
+    },
+  }),
+}
