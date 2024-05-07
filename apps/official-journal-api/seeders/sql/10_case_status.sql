5c08ac7a7538ff3b29b904e50e84d2ffaa5ede34
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2024-05-07 13:48:12 +0000

    Setting correct values to case status

--- apps/official-journal-api/seeders/sql/10_case_status.sql
+++ apps/official-journal-api/seeders/sql/10_case_status.sql
@@ -1,7 +1,7 @@
-INSERT INTO case_status (id, title, value) VALUES ('799722be-5530-439a-91dc-606e129b030d', 'Innsent', 'Innsent');
-INSERT INTO case_status (id, title, value) VALUES ('e926beb2-4001-4315-aed9-e4eec2ca963d', 'Grunnvinnsla', 'Grunnvinnsla');
-INSERT INTO case_status (id, title, value) VALUES ('22b038c1-1d8b-4a01-b061-02f22358d17e', 'Yfirlestur', 'Yfirlestur');
-INSERT INTO case_status (id, title, value) VALUES ('a90d65b7-905f-4ebb-aa1d-7fa115d91d56', 'Tilbúið', 'Tilbúið');
-INSERT INTO case_status (id, title, value) VALUES ('cd1536de-4481-492f-8db1-1ea0e3e38880', 'Útgefið', 'Útgefið');
-INSERT INTO case_status (id, title, value) VALUES ('c2b24d63-e5d9-417f-8ec1-ef5150e1a17f', 'Tekið úr birtingu', 'Tekið úr birtingu');
-INSERT INTO case_status (id, title, value) VALUES ('a8668645-ef72-4f2d-8e75-7bfe65b0123c', 'Birtingu hafnað', 'Birtingu hafnað');
+INSERT INTO case_status (id, key, value) VALUES ('799722be-5530-439a-91dc-606e129b030d', 'Submitted', 'Innsent');
+INSERT INTO case_status (id, key, value) VALUES ('e926beb2-4001-4315-aed9-e4eec2ca963d', 'InProgress', 'Grunnvinnsla');
+INSERT INTO case_status (id, key, value) VALUES ('22b038c1-1d8b-4a01-b061-02f22358d17e', 'InReview', 'Yfirlestur');
+INSERT INTO case_status (id, key, value) VALUES ('a90d65b7-905f-4ebb-aa1d-7fa115d91d56', 'ReadyForPublishing', 'Tilbúið');
+INSERT INTO case_status (id, key, value) VALUES ('cd1536de-4481-492f-8db1-1ea0e3e38880', 'Published', 'Útgefið');
+INSERT INTO case_status (id, key, value) VALUES ('c2b24d63-e5d9-417f-8ec1-ef5150e1a17f', 'Unpublished', 'Tekið úr birtingu');
+INSERT INTO case_status (id, key, value) VALUES ('a8668645-ef72-4f2d-8e75-7bfe65b0123c', 'Rejected', 'Birtingu hafnað');
