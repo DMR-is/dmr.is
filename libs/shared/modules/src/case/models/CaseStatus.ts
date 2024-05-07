b0e91e3820757e946bc49a2a6c95e11890fd1d9b
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2024-05-07 13:14:25 +0000

    CaseStatus model

---
+++ libs/shared/modules/src/case/models/CaseStatus.ts
@@ -0,0 +1,30 @@
+import { Model } from 'sequelize'
+import { Column, DataType, DefaultScope, Table } from 'sequelize-typescript'
+
+@Table({ tableName: 'case_status', timestamps: true })
+@DefaultScope(() => ({
+  attributes: {
+    exclude: ['created', 'updated'],
+  },
+}))
+export class CaseStatusDto extends Model {
+  @Column({
+    type: DataType.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+    defaultValue: DataType.UUIDV4,
+  })
+  id!: string
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  title!: string
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  value!: string
+}
