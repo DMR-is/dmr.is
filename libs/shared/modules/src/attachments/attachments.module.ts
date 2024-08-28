0fc286fb92e6c9d25b65aa16599045c324817c6e
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2024-08-28 11:48:11 +0000

    exporting attahcment service from the attachment module

--- libs/shared/modules/src/attachments/attachments.module.ts
+++ libs/shared/modules/src/attachments/attachments.module.ts
@@ -3,12 +3,19 @@ import { LoggingModule } from '@dmr.is/logging'
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 
+import { AttachmentService } from './attachment.service'
+import { IAttachmentService } from './attachment.service.interface'
 import { models as attachmentModels } from './models'
 
 @Module({
   imports: [SequelizeModule.forFeature([...attachmentModels]), LoggingModule],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [
+    {
+      provide: IAttachmentService,
+      useClass: AttachmentService,
+    },
+  ],
+  exports: [IAttachmentService],
 })
 export class AttachmentsModule {}
