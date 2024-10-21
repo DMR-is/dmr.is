741a668b35ba6ffd9bc5ae20aaec6f4000abc4f3
Valur Sverrisson <valur@hugsmidjan.is>
2024-10-21 16:19:10 +0000

    fix: incorrect type for CasesInProgress response

--- libs/shared/dto/src/lib/cases/get-cases-in-progress-response.dto.ts
+++ libs/shared/dto/src/lib/cases/get-cases-in-progress-response.dto.ts
@@ -1,12 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger'
 
 import { Paging } from '../paging/paging.dto'
-import { Case } from './case.dto'
 import { CaseInProgress } from './case-in-progress.dto'
 
 export class GetCasesInProgressReponse {
   @ApiProperty({
-    type: [Case],
+    type: [CaseInProgress],
   })
   cases!: CaseInProgress[]
 
