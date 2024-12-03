be09bdf0f0f85a15372de83cf6aaa0413f511ec6
Þórður H <tolleinn@gmail.com>
2024-12-03 09:44:12 +0000

    add guard to single case add max age to session (#264)
    * Categories guard logging test
    
    * Add max age to session idle time, add guard back
    
    * Remove logg
    
    * Cleanup

--- apps/official-journal-web/src/pages/api/auth/[...nextauth].ts
+++ apps/official-journal-web/src/pages/api/auth/[...nextauth].ts
@@ -16,6 +16,7 @@ type ErrorWithPotentialReqRes = Error & {
 }
 
 const NODE_ENV = process.env.NODE_ENV
+const SESION_TIMEOUT = 60 * 60 // 1 hour
 
 const secure = NODE_ENV === 'production' ? '__Secure-' : ''
 
@@ -63,6 +64,7 @@ export const authOptions: AuthOptions = {
   },
   session: {
     strategy: 'jwt',
+    maxAge: SESION_TIMEOUT,
   },
   callbacks: {
     jwt: async ({ token, user }) => {
