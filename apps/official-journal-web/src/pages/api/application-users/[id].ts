f5ed982734aeac48fa17d1f83fe71824c34f1e27
Jón Bjarni Ólafsson <92530555+jonbjarnio@users.noreply.github.com>
2025-01-09 11:22:40 +0000

    fix(web): Update tsconfig (#300)
    * Set module to esnext and target to 2015 same as island.is
    
    * Updated import to import type

--- apps/official-journal-web/src/pages/api/application-users/[id].ts
+++ apps/official-journal-web/src/pages/api/application-users/[id].ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { LogMethod } from '@dmr.is/decorators'
 import { logger } from '@dmr.is/logging'
 import { AuthMiddleware } from '@dmr.is/middleware'
