cd6d17097e6f59a9610f86cf2d66d1d17919fa59
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-01-20 09:26:53 +0000

    Updated the contorl panel, new dorpdown menu.

--- apps/official-journal-web/src/components/header/ControlPanel.css.ts
+++ apps/official-journal-web/src/components/header/ControlPanel.css.ts
@@ -1,38 +1,51 @@
 import { style } from '@vanilla-extract/css'
+import { recipe } from '@vanilla-extract/recipes'
 
 import { theme } from '@island.is/island-ui/theme'
 
 import { HEADER_HEIGHT } from '../../lib/constants'
 
 export const controlPanel = style({
+  height: HEADER_HEIGHT,
   inlineSize: 300,
+
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
-  alignItems: 'space-between',
-  height: HEADER_HEIGHT,
+
   borderLeft: `1px solid ${theme.color.blue200}`,
   borderRight: `1px solid ${theme.color.blue200}`,
+
   paddingInline: theme.spacing[3],
+  position: 'relative',
+  zIndex: 1,
 })
 
-export const controlPanelWrapper = style({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
+export const controlPanelChevron = recipe({
+  base: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: '50%',
+    padding: 4,
+  },
+  variants: {
+    color: {
+      blue: {
+        background: theme.color.blue100,
+      },
+      white: {
+        background: theme.color.white,
+      },
+    },
+  },
 })
 
-export const controlPanelTitle = style({
-  fontSize: 14,
-  fontWeight: theme.typography.semiBold,
-})
-
-export const controlPanelButton = style({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: 24,
-  height: 24,
-  borderRadius: '50%',
-  backgroundColor: theme.color.white,
+export const dropdownMenu = style({
+  position: 'relative',
+  overflow: 'hidden',
+  width: 300,
+  borderBottomLeftRadius: theme.border.radius.large,
+  borderBottomRightRadius: theme.border.radius.large,
 })
