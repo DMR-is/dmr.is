8096df545c253a8a968459142b6840bb1ffa75b4
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-01-29 11:37:32 +0000

    Database migrations and model

---
+++ libs/shared/modules/src/case/models/case-history.model.ts
@@ -0,0 +1,123 @@
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript'
+
+import { AdminUserModel } from '../../admin-user/models/admin-user.model'
+import { AdvertTypeModel } from '../../advert-type/models'
+import {
+  AdvertDepartmentModel,
+  AdvertInvolvedPartyModel,
+} from '../../journal/models'
+import { CaseModel } from './case.model'
+import { CaseStatusModel } from './case-status.model'
+
+@Table({ tableName: 'case_history', timestamps: false })
+export class CaseHistoryModel extends Model {
+  @PrimaryKey
+  @Column({
+    type: DataType.UUIDV4,
+    field: 'id',
+    allowNull: false,
+  })
+  override id!: string
+
+  @ForeignKey(() => CaseModel)
+  @Column({
+    type: DataType.UUIDV4,
+    field: 'case_id',
+    allowNull: false,
+  })
+  caseId!: string
+
+  @ForeignKey(() => AdvertDepartmentModel)
+  @Column({
+    type: DataType.UUID,
+    field: 'department_id',
+    allowNull: false,
+  })
+  departmentId!: string
+
+  @ForeignKey(() => AdvertTypeModel)
+  @Column({
+    type: DataType.UUID,
+    field: 'type_id',
+    allowNull: false,
+  })
+  typeId!: string
+
+  @ForeignKey(() => CaseStatusModel)
+  @Column({
+    type: DataType.UUID,
+    field: 'status_id',
+    allowNull: false,
+  })
+  statusId!: string
+
+  @ForeignKey(() => AdvertInvolvedPartyModel)
+  @Column({
+    type: DataType.UUID,
+    field: 'institution_id',
+    allowNull: false,
+  })
+  involvedPartyId!: string
+
+  @ForeignKey(() => AdminUserModel)
+  @Column({
+    type: DataType.UUID,
+    field: 'admin_user_id',
+    allowNull: true,
+  })
+  adminUserId!: string | null
+
+  @Column({
+    type: DataType.STRING,
+    field: 'title',
+    allowNull: false,
+  })
+  title!: string
+
+  @Column({
+    type: DataType.TEXT,
+    field: 'html',
+    allowNull: false,
+  })
+  html!: string
+
+  @Column({
+    type: DataType.STRING,
+    field: 'requested_publication_date',
+    allowNull: true,
+  })
+  requestedPublicationDate!: string | null
+
+  @Column({
+    type: DataType.STRING,
+    field: 'created_at',
+    allowNull: false,
+  })
+  created!: string
+
+  @BelongsTo(() => CaseModel)
+  case!: CaseModel
+
+  @BelongsTo(() => AdvertDepartmentModel)
+  department!: AdvertDepartmentModel
+
+  @BelongsTo(() => AdvertTypeModel)
+  type!: AdvertTypeModel
+
+  @BelongsTo(() => CaseStatusModel)
+  status!: CaseStatusModel
+
+  @BelongsTo(() => AdvertInvolvedPartyModel)
+  involvedParty!: AdvertInvolvedPartyModel
+
+  @BelongsTo(() => AdminUserModel)
+  adminUser!: AdminUserModel | null
+}
