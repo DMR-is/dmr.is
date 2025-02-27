c2f6817aca2d81ee201ac6b90120419c97531f47
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-02-27 09:24:37 +0000

    Updated swagger docs for admin-user controller

--- libs/shared/modules/src/admin-user/admin-user.controller.ts
+++ libs/shared/modules/src/admin-user/admin-user.controller.ts
@@ -1,8 +1,7 @@
 import { USER_ROLES } from '@dmr.is/constants'
-import { LogMethod, Roles } from '@dmr.is/decorators'
+import { Roles } from '@dmr.is/decorators'
 import { Logger, LOGGER_PROVIDER } from '@dmr.is/logging'
 import {
-  AdminUser,
   CreateAdminUser,
   GetAdminUser,
   GetAdminUserRoles,
@@ -24,10 +23,8 @@ import {
 } from '@nestjs/common'
 import {
   ApiBearerAuth,
-  ApiBody,
   ApiNoContentResponse,
   ApiOperation,
-  ApiParam,
   ApiResponse,
 } from '@nestjs/swagger'
 
@@ -35,11 +32,12 @@ import { RoleGuard, TokenJwtAuthGuard } from '../guards'
 import { IAdminUserService } from './admin-user.service.interface'
 
 @ApiBearerAuth()
-@UseGuards(TokenJwtAuthGuard, RoleGuard)
 @Controller({
   version: '1',
   path: 'admin-users',
 })
+@UseGuards(TokenJwtAuthGuard, RoleGuard)
+@Roles(USER_ROLES.Admin)
 export class AdminUserController {
   constructor(
     @Inject(IAdminUserService)
@@ -48,11 +46,9 @@ export class AdminUserController {
     @Inject(LOGGER_PROVIDER) private readonly logger: Logger,
   ) {}
 
-  @Roles(USER_ROLES.Admin)
   @Get('/roles')
   @ApiOperation({ operationId: 'getRoles' })
   @ApiResponse({ status: 200, type: GetAdminUserRoles })
-  @LogMethod()
   async getRoles() {
     const results = await this.adminUserService.getRoles()
 
@@ -63,11 +59,9 @@ export class AdminUserController {
     return results.result.value
   }
 
-  @Roles(USER_ROLES.Admin)
   @Get('/users')
   @ApiOperation({ operationId: 'getUsers' })
   @ApiResponse({ status: 200, type: GetAdminUsers })
-  @LogMethod()
   async getAllUsers() {
     const results = await this.adminUserService.getUsers()
 
@@ -78,12 +72,9 @@ export class AdminUserController {
     return results.result.value
   }
 
-  @Roles(USER_ROLES.Admin)
   @Post('/users')
   @ApiOperation({ operationId: 'createUser' })
-  @ApiBody({ type: CreateAdminUser })
   @ApiNoContentResponse()
-  @LogMethod()
   async createAdminUser(@Body() body: CreateAdminUser) {
     const results = await this.adminUserService.createAdminUser(body)
 
@@ -92,12 +83,9 @@ export class AdminUserController {
     }
   }
 
-  @Roles(USER_ROLES.Admin)
   @Get('/users/:id')
-  @ApiParam({ name: 'id', type: 'string' })
   @ApiOperation({ operationId: 'getUserById' })
   @ApiResponse({ status: 200, type: GetAdminUser })
-  @LogMethod()
   async getUserById(@Param('id') id: string) {
     const results = await this.adminUserService.getUserById(id)
 
@@ -108,12 +96,9 @@ export class AdminUserController {
     return results.result.value
   }
 
-  @Roles(USER_ROLES.Admin)
   @Get('/users/nationalId/:nationalId')
-  @ApiParam({ name: 'nationalId', type: 'string' })
   @ApiOperation({ operationId: 'getUserByNationalId' })
   @ApiResponse({ status: 200, type: GetAdminUser })
-  @LogMethod()
   async getUserByNationalId(@Param('nationalId') nationalId: string) {
     const results = await this.adminUserService.getUserByNationalId(nationalId)
 
@@ -124,13 +109,9 @@ export class AdminUserController {
     return results.result.value
   }
 
-  @Roles(USER_ROLES.Admin)
   @Put('/users/:id')
-  @ApiParam({ name: 'id', type: 'string' })
   @ApiOperation({ operationId: 'updateUser' })
   @ApiNoContentResponse()
-  @ApiBody({ type: UpdateAdminUser })
-  @LogMethod()
   async updateUser(@Param('id') id: string, @Body() body: UpdateAdminUser) {
     const results = await this.adminUserService.updateUser(id, body)
 
@@ -139,12 +120,9 @@ export class AdminUserController {
     }
   }
 
-  @Roles(USER_ROLES.Admin)
   @Delete('/users/:id')
-  @ApiParam({ name: 'id', type: 'string' })
   @ApiOperation({ operationId: 'deleteUser' })
   @ApiNoContentResponse()
-  @LogMethod()
   async deleteUser(@Param('id') id: string) {
     const results = await this.adminUserService.deleteUser(id)
 
