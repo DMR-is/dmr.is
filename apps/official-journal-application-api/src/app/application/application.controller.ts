148dc6a9a27acd517bcc3cdf0abb3512327e4cdd
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-02-27 10:06:11 +0000

    Updated swagger docs for application controller

--- apps/official-journal-application-api/src/app/application/application.controller.ts
+++ apps/official-journal-application-api/src/app/application/application.controller.ts
@@ -1,4 +1,4 @@
-import { CurrentUser, LogMethod, Route, WithCase } from '@dmr.is/decorators'
+import { CurrentUser, Route, WithCase } from '@dmr.is/decorators'
 import {
   ApplicationAuthGaurd,
   IApplicationService,
@@ -30,8 +30,8 @@ import 'multer'
 import {
   Body,
   Controller,
+  Delete,
   Get,
-  HttpCode,
   Inject,
   MaxFileSizeValidator,
   Param,
@@ -56,9 +56,9 @@ import {
 import {
   ApiBody,
   ApiConsumes,
+  ApiNoContentResponse,
   ApiOperation,
   ApiParam,
-  ApiQuery,
   ApiResponse,
 } from '@nestjs/swagger'
 import { Logger, LOGGER_PROVIDER } from '@dmr.is/logging'
@@ -67,9 +67,8 @@ import { Logger, LOGGER_PROVIDER } from '@dmr.is/logging'
   path: 'applications',
   version: '1',
 })
-/**
- * Controller class for handling application-related requests.
- */
+@UseGuards(ApplicationAuthGaurd)
+@WithCase(false)
 export class ApplicationController {
   constructor(
     @Inject(IApplicationService)
@@ -84,35 +83,18 @@ export class ApplicationController {
     @Inject(LOGGER_PROVIDER) private readonly logger: Logger,
   ) {}
 
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
   @Get('/adverts')
-  @ApiOperation({
-    operationId: 'getAdvertCopies',
-  })
-  @ApiQuery({ type: GetApplicationAdvertsQuery, required: false })
-  @ApiResponse({
-    type: GetApplicationResponse,
-  })
+  @ApiOperation({ operationId: 'getAdvertCopies' })
+  @ApiResponse({ type: GetApplicationResponse })
   async getAdvertCopies(@Query() query: GetApplicationAdvertsQuery) {
     return ResultWrapper.unwrap(
       await this.applicationService.getApplicationAdverts(query),
     )
   }
 
-  /**
-   * Retrieves the price of an application.
-   * @param applicationId The ID of the application.
-   * @returns A promise that resolves to the price of the application.
-   */
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    path: ':id/price',
-    operationId: 'getPrice',
-    params: [{ name: 'id', type: 'string', required: true }],
-    responseType: CasePriceResponse,
-  })
+  @Get(':id/price')
+  @ApiOperation({ operationId: 'getPrice' })
+  @ApiResponse({ type: CasePriceResponse })
   async getPrice(
     @Param('id', new UUIDValidationPipe()) applicationId: string,
   ): Promise<CasePriceResponse> {
@@ -121,19 +103,9 @@ export class ApplicationController {
     )
   }
 
-  /**
-   * Retrieves an application by its ID.
-   * @param id The ID of the application.
-   * @returns A promise that resolves to the application.
-   */
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    path: ':id',
-    operationId: 'getApplication',
-    params: [{ name: 'id', type: 'string', required: true }],
-    responseType: GetApplicationResponse,
-  })
+  @Get(':id')
+  @ApiOperation({ operationId: 'getApplication' })
+  @ApiResponse({ type: GetApplicationResponse })
   async getApplication(
     @Param('id', new UUIDValidationPipe()) id: string,
   ): Promise<GetApplicationResponse> {
@@ -142,19 +114,9 @@ export class ApplicationController {
     )
   }
 
-  /**
-   * Handles submissions from the application system.
-   * @param applicationId The ID of the application.
-   * @returns A promise that resolves when the application is posted.
-   */
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    method: 'post',
-    path: ':id/post',
-    operationId: 'postApplication',
-    params: [{ name: 'id', type: 'string', required: true }],
-  })
+  @Post(':id/post')
+  @ApiOperation({ operationId: 'postApplication' })
+  @ApiResponse({ status: 200 })
   async postApplication(
     @Param('id', new UUIDValidationPipe()) applicationId: string,
   ) {
@@ -163,43 +125,22 @@ export class ApplicationController {
     )
   }
 
-  /**
-   * Retrieves the comments of an application.
-   * @param applicationId The ID of the application.
-   * @returns A promise that resolves to the comments of the application.
-   */
-  @UseGuards(ApplicationAuthGaurd)
+  @Get(':id/comments')
+  @ApiOperation({ operationId: 'getComments' })
+  @ApiResponse({ status: 200, type: GetComments })
   @WithCase(true)
-  @Route({
-    path: ':id/comments',
-    operationId: 'getComments',
-    params: [{ name: 'id', type: 'string', required: true }],
-    responseType: GetComments,
-  })
   async getComments(
     @Param('id', new UUIDValidationPipe()) applicationId: string,
-    @CurrentUser() user: ApplicationUser,
   ): Promise<GetComments> {
     return ResultWrapper.unwrap(
       await this.applicationService.getComments(applicationId),
     )
   }
 
-  /**
-   * Posts a comment on an application.
-   * @param applicationId The ID of the application.
-   * @param commentBody The body of the comment.
-   * @returns A promise that resolves when the comment is posted.
-   */
-  @UseGuards(ApplicationAuthGaurd)
+  @Post(':id/comments')
+  @ApiOperation({ operationId: 'postComment' })
+  @ApiNoContentResponse()
   @WithCase(true)
-  @Route({
-    method: 'post',
-    path: ':id/comments',
-    operationId: 'postComment',
-    params: [{ name: 'id', type: 'string', required: true }],
-    bodyType: PostApplicationComment,
-  })
   async postComment(
     @Param('id', new UUIDValidationPipe()) applicationId: string,
     @Body() commentBody: PostApplicationComment,
@@ -215,15 +156,8 @@ export class ApplicationController {
   }
 
   @Post(':id/upload')
-  @ApiOperation({
-    operationId: 'uploadApplicationAttachment',
-  })
+  @ApiOperation({ operationId: 'uploadApplicationAttachment' })
   @ApiConsumes('multipart/form-data')
-  @ApiParam({
-    name: 'id',
-    type: 'string',
-    required: true,
-  })
   @ApiBody({
     description: 'Handles uploading attachments for an application.',
     required: true,
@@ -241,14 +175,8 @@ export class ApplicationController {
       },
     },
   })
-  @ApiResponse({
-    status: 200,
-    description: 'The attachments were uploaded successfully.',
-    type: S3UploadFilesResponse,
-  })
-  @HttpCode(200)
+  @ApiResponse({ status: 200, type: S3UploadFilesResponse })
   @UseInterceptors(FilesInterceptor('files'))
-  @LogMethod()
   async uploadApplicationAttachment(
     @Param('id', new UUIDValidationPipe()) applicationId: string,
     @UploadedFiles(
@@ -277,19 +205,10 @@ export class ApplicationController {
     )
   }
 
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    path: ':id/presigned-url/:type',
-    method: 'post',
-    operationId: 'getPresignedUrl',
-    params: [
-      { name: 'id', type: 'string', required: true },
-      { name: 'type', enum: AttachmentTypeParam, required: true },
-    ],
-    bodyType: GetPresignedUrlBody,
-    responseType: PresignedUrlResponse,
-  })
+  @Post(':id/presigned-url/:type')
+  @ApiOperation({ operationId: 'getPresignedUrl' })
+  @ApiParam({ name: 'type', enum: AttachmentTypeParam })
+  @ApiResponse({ type: PresignedUrlResponse })
   async getPresignedUrl(
     @Body() body: GetPresignedUrlBody,
     @Param('id', new UUIDValidationPipe()) applicationId: string,
@@ -306,18 +225,10 @@ export class ApplicationController {
     )
   }
 
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    path: ':id/attachments/:type',
-    method: 'post',
-    operationId: 'addApplicationAttachment',
-    params: [
-      { name: 'id', type: String, required: true },
-      { name: 'type', enum: AttachmentTypeParam, required: true },
-    ],
-    bodyType: PostApplicationAttachmentBody,
-  })
+  @Post(':id/attachments/:type')
+  @ApiOperation({ operationId: 'addApplicationAttachment' })
+  @ApiParam({ name: 'type', enum: AttachmentTypeParam })
+  @ApiNoContentResponse()
   async addApplicationAttachment(
     @Param('id', new UUIDValidationPipe()) applicationId: string,
     @Param('type', new EnumValidationPipe(AttachmentTypeParam))
@@ -333,17 +244,10 @@ export class ApplicationController {
     )
   }
 
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    path: ':id/attachments/:type',
-    params: [
-      { name: 'id', type: String, required: true },
-      { name: 'type', enum: AttachmentTypeParam, required: true },
-    ],
-    operationId: 'getApplicationAttachments',
-    responseType: GetApplicationAttachmentsResponse,
-  })
+  @Get(':id/attachments/:type')
+  @ApiOperation({ operationId: 'getApplicationAttachments' })
+  @ApiParam({ name: 'type', enum: AttachmentTypeParam })
+  @ApiResponse({ type: GetApplicationAttachmentsResponse })
   async getApplicationAttachments(
     @Param('id', new UUIDValidationPipe()) applicationId: string,
     @Param('type', new EnumValidationPipe(AttachmentTypeParam))
@@ -357,20 +261,9 @@ export class ApplicationController {
     )
   }
 
-  /**
-   *
-   * @param applicationId id of the application
-   * @param key location of the attachment in s3
-   */
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    path: ':id/attachments/',
-    method: 'delete',
-    operationId: 'deleteApplicationAttachment',
-    params: [{ name: 'id', type: String, required: true }],
-    query: [{ name: 'key', type: String, required: true }],
-  })
+  @Delete(':id/attachments/delete')
+  @ApiOperation({ operationId: 'deleteApplicationAttachment' })
+  @ApiNoContentResponse()
   async deleteApplicationAttachment(
     @Param('id', new UUIDValidationPipe()) applicationId: string,
     @Query('key', IsStringValidationPipe) key: string,
@@ -383,28 +276,19 @@ export class ApplicationController {
     )
   }
 
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    path: ':id/involved-parties',
-    operationId: 'getInvolvedParties',
-    params: [{ name: 'id', type: 'string', required: true }],
-    responseType: ApplicationUserInvolvedPartiesResponse,
-  })
+  @Get(':id/involved-parties')
+  @ApiOperation({ operationId: 'getInvolvedParties' })
+  @ApiResponse({ type: ApplicationUserInvolvedPartiesResponse })
   async getInvolvedParties(@CurrentUser() user: ApplicationUser) {
     return ResultWrapper.unwrap(
       await this.applicationUserService.getUserInvolvedParties(user.id),
     )
   }
 
-  @UseGuards(ApplicationAuthGaurd)
+  @Get(':id/case')
+  @ApiOperation({ operationId: 'getApplicationCase' })
+  @ApiResponse({ type: GetApplicationCaseResponse })
   @WithCase(true)
-  @Route({
-    path: ':id/case',
-    operationId: 'getApplicationCase',
-    params: [{ name: 'id', type: 'string', required: true }],
-    responseType: GetApplicationCaseResponse,
-  })
   async getApplicationCase(
     @Param('id', new UUIDValidationPipe()) applicationId: string,
   ) {
@@ -413,29 +297,19 @@ export class ApplicationController {
     )
   }
 
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    path: 'advert/templates',
-    method: 'get',
-    operationId: 'getApplicationAdvertTemplates',
-    responseType: [AdvertTemplateDetails],
-  })
+  @Get('advert/templates')
+  @ApiOperation({ operationId: 'getApplicationAdvertTemplates' })
+  @ApiResponse({ type: [AdvertTemplateDetails] })
   async getApplicationAdvertTemplates() {
     return ResultWrapper.unwrap(
       await this.applicationService.getApplicationAdvertTemplates(),
     )
   }
 
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @Route({
-    path: 'advert/templates/:advertType',
-    method: 'get',
-    operationId: 'getApplicationAdvertTemplate',
-    params: [{ name: 'advertType', type: 'string', required: true }],
-    responseType: GetAdvertTemplateResponse,
-  })
+  @Get('advert/templates/:advertType')
+  @ApiOperation({ operationId: 'getApplicationAdvertTemplate' })
+  @ApiParam({ name: 'advertType', enum: AdvertTemplateTypeEnums })
+  @ApiResponse({ type: GetAdvertTemplateResponse })
   async getApplicationAdvertTemplate(
     @Param('advertType') advertType: AdvertTemplateTypeEnums,
   ) {
@@ -446,24 +320,10 @@ export class ApplicationController {
     )
   }
 
-  @UseGuards(ApplicationAuthGaurd)
-  @WithCase(false)
-  @ApiResponse({
-    status: 404,
-    description: 'Signature not found',
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'Successful signature get.',
-    type: GetSignature,
-  })
-  @Route({
-    path: '/involved-party/:involvedPartyId',
-    method: 'get',
-    operationId: 'getSignaturesForInvolvedParty',
-    params: [{ name: 'involvedPartyId', type: 'string', required: true }],
-    responseType: GetSignature,
-  })
+  @Get('involved-party/:involvedPartyId')
+  @ApiOperation({ operationId: 'getSignaturesForInvolvedParty' })
+  @ApiResponse({ status: 200, type: GetSignature })
+  @ApiResponse({ status: 404 })
   async getSignaturesForInvolvedParty(
     @Param('involvedPartyId', new UUIDValidationPipe()) involvedPartyId: string,
   ) {
