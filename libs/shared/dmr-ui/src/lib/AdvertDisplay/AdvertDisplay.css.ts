b92710385c193bf16824fb735138715518316731
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-02-28 10:06:05 +0000

    Advert display component

---
+++ libs/shared/dmr-ui/src/lib/AdvertDisplay/AdvertDisplay.css.ts
@@ -0,0 +1,40 @@
+import { globalStyle, style } from '@vanilla-extract/css'
+
+import { theme } from '@island.is/island-ui/theme'
+import {
+  regulationContentStyling,
+  regulationTitleStyling,
+} from '@island.is/regulations/styling'
+
+export const bodyText = style({})
+regulationContentStyling(bodyText)
+regulationTitleStyling(bodyText)
+
+export const modalBackground = style({
+  height: '100vh',
+  display: 'grid',
+  gridTemplateRows: 'auto 1fr',
+  rowGap: theme.spacing[2],
+  padding: theme.spacing[2],
+  backgroundColor: `${theme.color.blue100}00`,
+})
+
+globalStyle(
+  `
+    .section__title em,
+    .section__title i,
+    .chapter__title em,
+    .chapter__title i,
+    .subchapter__title em,
+    .subchapter__title i,
+    .article__title em,
+    .article__title i`,
+  {
+    display: 'block',
+  },
+)
+
+export const wrapper = style({
+  position: 'relative',
+  overflowY: 'auto',
+})
