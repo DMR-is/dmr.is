851a05376bf91172f3139f71f0c4844238c96345
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-03 15:14:03 +0000

    feat(dmr-ui): Tracker table (#367)
    * Added section component, added hypen to link cards
    
    * add Wrapper component
    
    * add TrackerTable component
    
    * Added piechart component
    
    * Fixed merge changes

--- apps/official-journal-web/src/pages/tmp/playground.tsx
+++ apps/official-journal-web/src/pages/tmp/playground.tsx
@@ -1,6 +1,18 @@
-import { Hero, LinkCard, Section } from '@dmr.is/ui'
+import {
+  Hero,
+  LinkCard,
+  PieChart,
+  Section,
+  TrackerTable,
+  Wrapper,
+} from '@dmr.is/ui'
 
-import { GridColumn, GridRow } from '@island.is/island-ui/core'
+import {
+  GridColumn,
+  GridContainer,
+  GridRow,
+  Stack,
+} from '@island.is/island-ui/core'
 
 export default function PlayGroundPage() {
   return (
@@ -47,7 +59,69 @@ export default function PlayGroundPage() {
         </GridRow>
       </Hero>
       <Section bleed variant="blue">
-        <h2>Hallo</h2>
+        <GridContainer>
+          <GridRow>
+            <GridColumn span={['12/12', '7/12']}>
+              <Stack space={3}>
+                <Wrapper title="Ritstjórn" link="#" linkText="Opna ritstjórn">
+                  <TrackerTable
+                    rows={[
+                      { text: '12 innsend mál hafa ekki verið opnuð' },
+                      { text: 'Borist hafa ný svör í 4 málum' },
+                      { text: '4 innsend mál eru með ósk um hraðbirtingu' },
+                      {
+                        text: '0 mál í yfirlestri eru með ósk um hraðbirtingu',
+                      },
+                    ]}
+                  />
+                </Wrapper>
+                <Wrapper title="Útgáfa" link="#" linkText="Opna útgáfuferli">
+                  <TrackerTable
+                    rows={[
+                      { text: '9 tilbúin mál eru áætluð til útgáfu í dag.' },
+                      {
+                        text: '2 mál í yfirlestri eru með liðinn birtingardag.',
+                      },
+                    ]}
+                  />
+                </Wrapper>
+              </Stack>
+            </GridColumn>
+            <GridColumn span={['12/12', '5/12']}>
+              <Wrapper title="Tölfræði" link="#" linkText="Sjá alla tölfræði">
+                <PieChart
+                  intro="Staða óútgefinna mála."
+                  items={[
+                    {
+                      color: 'dark400',
+                      title: 'Innsent',
+                      count: 1,
+                      percentage: 50,
+                    },
+                    {
+                      color: 'blue400',
+                      title: 'Grunnvinnsla',
+                      count: 2,
+                      percentage: 50,
+                    },
+                    {
+                      color: 'mint400',
+                      title: 'Yfirlestur',
+                      count: 1,
+                      percentage: 50,
+                    },
+                    {
+                      color: 'roseTinted400',
+                      title: 'Tilbúið',
+                      count: 1,
+                      percentage: 50,
+                    },
+                  ]}
+                />
+              </Wrapper>
+            </GridColumn>
+          </GridRow>
+        </GridContainer>
       </Section>
     </main>
   )
--- libs/shared/dmr-ui/src/index.ts
+++ libs/shared/dmr-ui/src/index.ts
@@ -1,5 +1,13 @@
+// components
 export * from './lib/AdvertDisplay/AdvertDisplay'
 export * from './lib/Hero/Hero'
 export * from './lib/Image/Image'
 export * from './lib/LinkCard/LinkCard'
 export * from './lib/Section/Section'
+export * from './lib/Wrapper/Wrapper'
+
+// tables
+export * from './lib/Tables/TrackerTable/TrackerTable'
+
+// charts
+export * from './lib/PieChart/PieChart'
