d6a54fcb8468ad8e3876bb87e5f16575e320bb1a
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-04 10:20:46 +0000

    Added layout

--- apps/legal-gazette-web/src/pages/_app.tsx
+++ apps/legal-gazette-web/src/pages/_app.tsx
@@ -2,12 +2,17 @@ import { AppProps as NextAppProps } from 'next/app'
 
 import { globalStyles } from '@island.is/island-ui/core'
 
+import { Layout } from '../layout/Layout'
+
 globalStyles()
+
 function CustomApp({ Component, pageProps }: NextAppProps) {
   return (
     <>
       <main className="app">
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </main>
     </>
   )
