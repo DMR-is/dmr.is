4279e2e6e4b3038ef6e151ff754beaa69c4f8b0f
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-17 13:18:19 +0000

    feat(ojoi): UI for communication channels (#391)
    Added ui for case communication channels

---
+++ apps/official-journal-web/src/components/case-update-fields/CommunicationChannelsField.tsx
@@ -0,0 +1,41 @@
+import { DataTable } from '@dmr.is/ui'
+
+import { AccordionItem } from '@island.is/island-ui/core'
+
+import { useCaseContext } from '../../hooks/useCaseContext'
+
+type Props = {
+  toggle: boolean
+  onToggle: () => void
+}
+
+export const CommunicationChannelsField = ({ toggle, onToggle }: Props) => {
+  const { currentCase } = useCaseContext()
+  return (
+    <AccordionItem
+      id="communication-channel-fields"
+      expanded={toggle}
+      onToggle={onToggle}
+      label="Samskiptaleiðir"
+      labelVariant="h5"
+      iconVariant="small"
+    >
+      <DataTable
+        columns={[
+          {
+            field: 'email',
+            children: 'Netfang',
+          },
+          {
+            field: 'phone',
+            children: 'Símanúmer',
+          },
+        ]}
+        rows={currentCase.channels.map((ch) => ({
+          email: ch.email,
+          phone: ch.phone,
+        }))}
+      ></DataTable>
+    </AccordionItem>
+  )
+}
