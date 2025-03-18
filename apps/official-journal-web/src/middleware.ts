60e8cd22a8027f83a2d774d77b6ffd11c3d3c4ba
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-18 10:20:54 +0000

    Fixed import in middleware

--- apps/official-journal-web/src/middleware.ts
+++ apps/official-journal-web/src/middleware.ts
@@ -2,38 +2,47 @@ import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
-import { Routes } from './lib/constants'
+const protectedRoutes = [
+  '/',
+  '/yfirflokkar',
+  '/tegundir',
+  '/ritstjorn',
+  '/ritstjorn/:caseId',
+  '/ritstjorn/:caseId/innsent',
+  '/ritstjorn/:caseId/grunnvinnsla',
+  '/ritstjorn/:caseId/yfirlestur',
+  '/ritstjorn/:caseId/tilbuid',
+  '/ritstjorn/:caseId/leidretting',
+  '/utgafa',
+  '/utgafa/stadfesting',
+  '/heildaryfirlit',
+  '/heildaryfirlit/:caseId',
+]
 
 export default async function middleware(req: NextRequest) {
   const token = await getToken({ req })
-
-  const {
-    Login: _login,
-    UserManagement: _userManagement,
-    ...protectedRouteObj
-  } = Routes
-
-  const protectedRoutes: string[] = Array.from(Object.values(protectedRouteObj))
 
   if (!protectedRoutes.includes(req.nextUrl.pathname)) {
     return NextResponse.next()
   }
 
   if (!token) {
-    return NextResponse.redirect(new URL(Routes.Login, req.url))
+    return NextResponse.redirect(new URL('/innskraning', req.url))
   }
 
   const isAdmin = token.user?.role.slug === 'ritstjori'
 
-  if (!isAdmin && req.nextUrl.pathname === Routes.UserManagement) {
+  if (!isAdmin && req.nextUrl.pathname === '/notendur') {
     return NextResponse.next() // Allow access to this page
   }
 
   if (!isAdmin) {
-    return NextResponse.redirect(new URL(Routes.UserManagement, req.url))
+    return NextResponse.redirect(new URL('/notendur', req.url))
   }
 
   return NextResponse.next()
 }
 
-export const config = { matcher: '/((?!.*\\.).*)' }
+export const config = {
+  matcher: '/((?!.*\\.).*)',
+}
