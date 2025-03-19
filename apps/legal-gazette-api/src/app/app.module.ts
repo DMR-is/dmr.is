f4f4e9e4b0ca43c7faba4ca835db4394678a5d5f
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-19 15:01:13 +0000

    Add global logging interceptor to the api

--- apps/legal-gazette-api/src/app/app.module.ts
+++ apps/legal-gazette-api/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
+import { APP_INTERCEPTOR } from '@nestjs/core'
+import { LoggingInterceptor } from '@dmr.is/shared/interceptors'
 import { AppService } from './app.service'
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
