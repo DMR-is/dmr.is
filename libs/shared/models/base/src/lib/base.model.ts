9824f76dbdb49bd671ef4e4c557d2891f8947ae7
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-19 15:17:17 +0000

    Creating base model where other models can extend from

---
+++ libs/shared/models/base/src/lib/base.model.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreatedAt,
+  DataType,
+  DeletedAt,
+  Model,
+  PrimaryKey,
+  UpdatedAt,
+} from 'sequelize-typescript'
+
+interface BaseAttributes {
+  id: string
+  created: Date
+  updated: Date
+  deleted: Date | null
+}
+
+type BaseModelAttributes<T> = T & BaseAttributes
+
+export class BaseModel<T> extends Model<BaseModelAttributes<T>> {
+  @PrimaryKey
+  @Column({
+    type: DataType.UUID,
+  })
+  id!: string
+
+  @CreatedAt
+  created!: Date
+
+  @UpdatedAt
+  updated!: Date
+
+  @DeletedAt
+  deleted!: Date | null
+}
