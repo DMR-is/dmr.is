c80d46771224510b2e6de54dc24ae9904a408e0d
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-27 10:42:57 +0000

    Utility module

---
+++ libs/official-journal/modules/utlity/src/index.ts
@@ -0,0 +1,6 @@
+// services
+export { IUtilityService } from './utility.service.interface'
+export { UtilityService } from './utility.service'
+
+// modules
+export { UtilityModule } from './utility.module'
