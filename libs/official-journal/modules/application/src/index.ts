091826813f77dda7f401066c19eef36ebea84afe
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-27 10:59:31 +0000

    Project structe for attachmetns

--- libs/official-journal/modules/application/src/index.ts
+++ libs/official-journal/modules/application/src/index.ts
@@ -3,42 +3,42 @@ export {
   ApplicationAddition,
   ApplicationAdvert,
   ApplicationCommunicationChannel,
-} from './dto/application-advert.dto'
-export { ApplicationAnswers } from './dto/application-answers.dto'
+} from './lib/dto/application-advert.dto'
+export { ApplicationAnswers } from './lib/dto/application-answers.dto'
 export {
   TransactionFeeCode,
   TransactionFeeCodesResponse,
-} from './dto/application-base-fee.dto'
-export { ApplicationMisc } from './dto/application-misc'
+} from './lib/dto/application-base-fee.dto'
+export { ApplicationMisc } from './lib/dto/application-misc'
 export {
   ApplicationSignatureMember,
   ApplicationSignatureRecord,
   ApplicationSignatureRecords,
   ApplicationSignatures,
-} from './dto/application-signature.dto'
-export { Application } from './dto/application.dto'
+} from './lib/dto/application-signature.dto'
+export { Application } from './lib/dto/application.dto'
 export {
   ApplicationAdvertItem,
   GetApplicationAdverts,
   GetApplicationAdvertsQuery,
-} from './dto/get-application-advert.dto'
-export { GetApplicationResponse } from './dto/get-application-response.dto'
-export { PostApplicationBody } from './dto/post-application-body.dto'
-export { PostApplicationComment } from './dto/post-application-comment.dto'
-export { PostApplicationResponse } from './dto/post-application-response.dto'
+} from './lib/dto/get-application-advert.dto'
+export { GetApplicationResponse } from './lib/dto/get-application-response.dto'
+export { PostApplicationBody } from './lib/dto/post-application-body.dto'
+export { PostApplicationComment } from './lib/dto/post-application-comment.dto'
+export { PostApplicationResponse } from './lib/dto/post-application-response.dto'
 export {
   UpdateApplicationAdvertDto,
   UpdateApplicationAnswersBody,
-} from './dto/update-application.answers.dto'
-export { UpdateApplicationBody } from './dto/updateApplication-body.dto'
+} from './lib/dto/update-application.answers.dto'
+export { UpdateApplicationBody } from './lib/dto/updateApplication-body.dto'
 
 // migrations
-export { applicationAdvertMigrate } from './migrations/application-advert.migrate'
-export { applicationCaseMigrate } from './migrations/application-case.migrate'
+export { applicationAdvertMigrate } from './lib/migrations/application-advert.migrate'
+export { applicationCaseMigrate } from './lib/migrations/application-case.migrate'
 
 // services
-export { ApplicationService } from './application.service'
-export { IApplicationService } from './application.service.interface'
+export { ApplicationService } from './lib/application.service'
+export { IApplicationService } from './lib/application.service.interface'
 
 // module
-export { ApplicationModule } from './application.module'
+export { ApplicationModule } from './lib/application.module'
--- libs/official-journal/modules/attachment/src/index.ts
+++ libs/official-journal/modules/attachment/src/index.ts
@@ -1,30 +1,30 @@
 // dto
-export { ApplicationAttachmentType } from './dto/application-attachment-type.dto'
-export { ApplicationAttachment } from './dto/application-attachment.dto'
-export { ApplicationAttachments } from './dto/application-attachments.dto'
-export { ApplicationCase } from './dto/application-case.dto'
-export { DeleteApplicationAttachmentBody } from './dto/delete-application-attachment-body.dto'
-export { GetApplicationAttachmentResponse } from './dto/get-application-attachment.response'
-export { GetApplicationAttachmentsResponse } from './dto/get-application-attachments.response'
-export { GetApplicationCaseResponse } from './dto/get-application-case.response'
+export { ApplicationAttachmentType } from './lib/dto/application-attachment-type.dto'
+export { ApplicationAttachment } from './lib/dto/application-attachment.dto'
+export { ApplicationAttachments } from './lib/dto/application-attachments.dto'
+export { ApplicationCase } from './lib/dto/application-case.dto'
+export { DeleteApplicationAttachmentBody } from './lib/dto/delete-application-attachment-body.dto'
+export { GetApplicationAttachmentResponse } from './lib/dto/get-application-attachment.response'
+export { GetApplicationAttachmentsResponse } from './lib/dto/get-application-attachments.response'
+export { GetApplicationCaseResponse } from './lib/dto/get-application-case.response'
 export {
   PostApplicationAssetBody,
   PostApplicationAttachmentBody,
-} from './dto/post-application-attachment.body'
+} from './lib/dto/post-application-attachment.body'
 
 // migrations
-export { attachmentMigrate } from './migrations/attachment.migration'
+export { attachmentMigrate } from './lib/migrations/attachment.migration'
 
 // models
-export { AdvertAttachmentsModel } from './models/advert-attachments.model'
-export { ApplicationAttachmentTypeModel } from './models/application-attachment-type.model'
-export { ApplicationAttachmentModel } from './models/application-attachment.model'
-export { ApplicationAttachmentsModel } from './models/application-attachments.model'
-export { CaseAttachmentsModel } from './models/case-attachments.model'
+export { AdvertAttachmentsModel } from './lib/models/advert-attachments.model'
+export { ApplicationAttachmentTypeModel } from './lib/models/application-attachment-type.model'
+export { ApplicationAttachmentModel } from './lib/models/application-attachment.model'
+export { ApplicationAttachmentsModel } from './lib/models/application-attachments.model'
+export { CaseAttachmentsModel } from './lib/models/case-attachments.model'
 
 // services
-export { AttachmentService } from './attachment.service'
-export { IAttachmentService } from './attachment.service.interface'
+export { AttachmentService } from './lib/attachment.service'
+export { IAttachmentService } from './lib/attachment.service.interface'
 
 // modules
-export { AttachmentModule } from './attachment.module'
+export { AttachmentModule } from './lib/attachment.module'
