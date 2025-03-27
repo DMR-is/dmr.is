83ee19f7e0d0591df678999aff794c99abafd817
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-27 11:00:45 +0000

    Project structure for case module

--- libs/official-journal/modules/case/src/index.ts
+++ libs/official-journal/modules/case/src/index.ts
@@ -2,9 +2,9 @@
 export {
   AddCaseAdvertCorrection,
   DeleteCaseAdvertCorrection,
-} from './dto/add-case-advert-correction.dto'
-export { CaseAddition } from './dto/case-addition.dto'
-export { CaseChannel, CreateCaseChannelDto } from './dto/case-channel.dto'
+} from './lib/dto/add-case-advert-correction.dto'
+export { CaseAddition } from './lib/dto/case-addition.dto'
+export { CaseChannel, CreateCaseChannelDto } from './lib/dto/case-channel.dto'
 export {
   AdditionType,
   CaseActionEnum,
@@ -13,16 +13,16 @@ export {
   CaseTagEnum,
   DepartmentEnum,
   DepartmentSlugEnum,
-} from './dto/case-constants'
-export { CaseHistory } from './dto/case-history.dto'
-export { CaseInProgress } from './dto/case-in-progress.dto'
+} from './lib/dto/case-constants'
+export { CaseHistory } from './lib/dto/case-history.dto'
+export { CaseInProgress } from './lib/dto/case-in-progress.dto'
 export {
   PaymentExpenses,
   PaymentExtraData,
   UpdateCasePaymentBody,
-} from './dto/case-payment-body.dto'
-export { CaseStatus } from './dto/case-status.dto'
-export { CaseTransaction } from './dto/case-transaction.dto'
+} from './lib/dto/case-payment-body.dto'
+export { CaseStatus } from './lib/dto/case-status.dto'
+export { CaseTransaction } from './lib/dto/case-transaction.dto'
 export {
   Case,
   CaseDetailed,
@@ -30,89 +30,89 @@ export {
   GetCasesWithDepartmentCount,
   GetCasesWithStatusCount,
   StatusCounter,
-} from './dto/case.dto'
-export { CommunicationStatus } from './dto/communication-status.dto'
-export { CreateCaseBody } from './dto/create-case-body.dto'
-export { CreateCaseChannelBody } from './dto/create-case-channel-body.dto'
-export { CreateCaseResponse } from './dto/create-case-response.dto'
-export { CreateCaseDto, CreateCaseResponseDto } from './dto/create-case.dto'
+} from './lib/dto/case.dto'
+export { CommunicationStatus } from './lib/dto/communication-status.dto'
+export { CreateCaseBody } from './lib/dto/create-case-body.dto'
+export { CreateCaseChannelBody } from './lib/dto/create-case-channel-body.dto'
+export { CreateCaseResponse } from './lib/dto/create-case-response.dto'
+export { CreateCaseDto, CreateCaseResponseDto } from './lib/dto/create-case.dto'
 export {
   GetPaymentQuery,
   GetPaymentResponse,
-} from './dto/get-case-payment-response.dto'
-export { CasePriceResponse } from './dto/get-case-price-response.dto'
-export { GetCaseResponse } from './dto/get-case-response.dto'
-export { GetCasesInProgressReponse } from './dto/get-cases-in-progress-response.dto'
-export { GetCasesQuery } from './dto/get-cases-query.dto'
-export { GetCasesReponse } from './dto/get-cases-response.dto'
+} from './lib/dto/get-case-payment-response.dto'
+export { CasePriceResponse } from './lib/dto/get-case-price-response.dto'
+export { GetCaseResponse } from './lib/dto/get-case-response.dto'
+export { GetCasesInProgressReponse } from './lib/dto/get-cases-in-progress-response.dto'
+export { GetCasesQuery } from './lib/dto/get-cases-query.dto'
+export { GetCasesReponse } from './lib/dto/get-cases-response.dto'
 export {
   GetCasesWithDepartmentCountQuery,
   GetCasesWithStatusCountQuery,
-} from './dto/get-cases-with-count-query.dto'
+} from './lib/dto/get-cases-with-count-query.dto'
 export {
   GetCasesWithPublicationNumber,
   GetCasesWithPublicationNumberQuery,
-} from './dto/get-cases-with-publication-number.dto'
-export { GetCommunicationSatusesResponse } from './dto/get-communication-satuses-response.dto'
-export { GetNextPublicationNumberResponse } from './dto/get-next-publication-number-response.dto'
-export { GetTagsResponse } from './dto/get-tags-response.dto'
-export { PostCasePublishBody } from './dto/post-publish-body.dto'
-export { CaseTag } from './dto/tag.dto'
+} from './lib/dto/get-cases-with-publication-number.dto'
+export { GetCommunicationSatusesResponse } from './lib/dto/get-communication-satuses-response.dto'
+export { GetNextPublicationNumberResponse } from './lib/dto/get-next-publication-number-response.dto'
+export { GetTagsResponse } from './lib/dto/get-tags-response.dto'
+export { PostCasePublishBody } from './lib/dto/post-publish-body.dto'
+export { CaseTag } from './lib/dto/tag.dto'
 export {
   UpdateAdvertHtmlBody,
   UpdateAdvertHtmlCorrection,
-} from './dto/update-advert-html-body.dto'
-export { UpdateCaseBody } from './dto/update-case-body.dto'
-export { UpdateCaseCommunicationBody } from './dto/update-case-communication-body.dto'
-export { UpdateCaseStatusBody } from './dto/update-case-status-body.dto'
-export { UpdateCategoriesBody } from './dto/update-category-body.dto'
-export { UpdateCommunicationStatusBody } from './dto/update-communication-status.dto'
-export { UpdateCaseDepartmentBody } from './dto/update-department-body.dto'
-export { UpdateFasttrackBody } from './dto/update-fasttrack-body.dto'
-export { UpdateNextStatusBody } from './dto/update-next-status-body.dto'
+} from './lib/dto/update-advert-html-body.dto'
+export { UpdateCaseBody } from './lib/dto/update-case-body.dto'
+export { UpdateCaseCommunicationBody } from './lib/dto/update-case-communication-body.dto'
+export { UpdateCaseStatusBody } from './lib/dto/update-case-status-body.dto'
+export { UpdateCategoriesBody } from './lib/dto/update-category-body.dto'
+export { UpdateCommunicationStatusBody } from './lib/dto/update-communication-status.dto'
+export { UpdateCaseDepartmentBody } from './lib/dto/update-department-body.dto'
+export { UpdateFasttrackBody } from './lib/dto/update-fasttrack-body.dto'
+export { UpdateNextStatusBody } from './lib/dto/update-next-status-body.dto'
 export {
   CaseFeeCalculationBody,
   UpdateCasePriceBody,
-} from './dto/update-price-body.dto'
-export { UpdatePublishDateBody } from './dto/update-publish-date-body.dto'
-export { UpdateTagBody } from './dto/update-tag-body.dto'
-export { UpdateTitleBody } from './dto/update-title-body.dto'
-export { UpdateCaseTypeBody } from './dto/update-type-body.dto'
+} from './lib/dto/update-price-body.dto'
+export { UpdatePublishDateBody } from './lib/dto/update-publish-date-body.dto'
+export { UpdateTagBody } from './lib/dto/update-tag-body.dto'
+export { UpdateTitleBody } from './lib/dto/update-title-body.dto'
+export { UpdateCaseTypeBody } from './lib/dto/update-type-body.dto'
 
 // migrations
-export { caseAdditionMigrate } from './migrations/case-addition.migrate'
-export { caseChannelMigrate } from './migrations/case-channel.migrate'
-export { caseCommunicationStatusMigrate } from './migrations/case-communication-status.migrate'
-export { caseDetailedMigrate } from './migrations/case-detailed.migrate'
-export { caseHistoryMigrate } from './migrations/case-history.migrate'
-export { caseStatusMigrate } from './migrations/case-status.migrate'
-export { caseTagMigrate } from './migrations/case-tag.migrate'
-export { caseTransactionMigrate } from './migrations/case-transaction.migrate'
-export { caseMigrate } from './migrations/case.migrate'
-export { transactionFeeCodeMigrate } from './migrations/transaction-fee-codes.migrate'
+export { caseAdditionMigrate } from './lib/migrations/case-addition.migrate'
+export { caseChannelMigrate } from './lib/migrations/case-channel.migrate'
+export { caseCommunicationStatusMigrate } from './lib/migrations/case-communication-status.migrate'
+export { caseDetailedMigrate } from './lib/migrations/case-detailed.migrate'
+export { caseHistoryMigrate } from './lib/migrations/case-history.migrate'
+export { caseStatusMigrate } from './lib/migrations/case-status.migrate'
+export { caseTagMigrate } from './lib/migrations/case-tag.migrate'
+export { caseTransactionMigrate } from './lib/migrations/case-transaction.migrate'
+export { caseMigrate } from './lib/migrations/case.migrate'
+export { transactionFeeCodeMigrate } from './lib/migrations/transaction-fee-codes.migrate'
 
 // models
-export { CaseAdditionModel } from './models/case-addition.model'
-export { CaseAdditionsModel } from './models/case-additions.model'
-export { CaseCategoriesModel } from './models/case-categories.model'
-export { CaseChannelModel } from './models/case-channel.model'
-export { CaseChannelsModel } from './models/case-channels.model'
-export { CaseCommunicationStatusModel } from './models/case-communication-status.model'
-export { CaseHistoryModel } from './models/case-history.model'
-export { CasePublishedAdvertsModel } from './models/case-published-adverts'
-export { CaseStatusModel } from './models/case-status.model'
-export { CaseTagModel } from './models/case-tag.model'
-export { CaseTransactionModel } from './models/case-transaction.model'
-export { CaseModel } from './models/case.model'
-export { TransactionFeeCodesModel } from './models/transaction-fee-codes.model'
+export { CaseAdditionModel } from './lib/models/case-addition.model'
+export { CaseAdditionsModel } from './lib/models/case-additions.model'
+export { CaseCategoriesModel } from './lib/models/case-categories.model'
+export { CaseChannelModel } from './lib/models/case-channel.model'
+export { CaseChannelsModel } from './lib/models/case-channels.model'
+export { CaseCommunicationStatusModel } from './lib/models/case-communication-status.model'
+export { CaseHistoryModel } from './lib/models/case-history.model'
+export { CasePublishedAdvertsModel } from './lib/models/case-published-adverts'
+export { CaseStatusModel } from './lib/models/case-status.model'
+export { CaseTagModel } from './lib/models/case-tag.model'
+export { CaseTransactionModel } from './lib/models/case-transaction.model'
+export { CaseModel } from './lib/models/case.model'
+export { TransactionFeeCodesModel } from './lib/models/transaction-fee-codes.model'
 
 // services
-export { CaseCreateService } from './services/create/case-create.service'
-export { ICaseCreateService } from './services/create/case-create.service.interface'
-export { CaseUpdateService } from './services/update/case-update.service'
-export { ICaseUpdateService } from './services/update/case-update.service.interface'
-export { CaseService } from './case.service'
-export { ICaseService } from './case.service.interface'
+export { CaseCreateService } from './lib/services/create/case-create.service'
+export { ICaseCreateService } from './lib/services/create/case-create.service.interface'
+export { CaseUpdateService } from './lib/services/update/case-update.service'
+export { ICaseUpdateService } from './lib/services/update/case-update.service.interface'
+export { CaseService } from './lib/case.service'
+export { ICaseService } from './lib/case.service.interface'
 
 // module
-export { CaseModule } from './case.module'
+export { CaseModule } from './lib/case.module'
