97fd74f206297a6a3580bd44755a1d3d4fd1b3d0
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-03-28 22:49:53 +0000

    Moving journal module to its appropriate location under official-journal-api

--- apps/official-journal-api/src/app/journal/journal.module.ts
+++ apps/official-journal-api/src/app/journal/journal.module.ts
@@ -1,15 +1,43 @@
 import {
-  AdvertTypeController,
-  AdvertTypeModule,
-} from '@dmr.is/official-journal/modules/advert-type'
-import { CaseModule } from '@dmr.is/official-journal/modules/case'
-import { JournalModule as MainModule } from '@dmr.is/official-journal/modules/journal'
+  AdvertCategoriesModel,
+  AdvertCategoryCategoriesModel,
+  AdvertCategoryModel,
+  AdvertDepartmentModel,
+  AdvertInvolvedPartyModel,
+  AdvertMainCategoryModel,
+  AdvertModel,
+  AdvertStatusModel,
+} from '@dmr.is/official-journal/models'
 
 import { Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize'
 
-import { JournalController } from './journal.controller'
+import { JournalService } from './journal.service'
+import { IJournalService } from './journal.service.interface'
+import { MockJournalService } from './journal.service.mock'
+
+const MOCK_DATA = process.env.API_MOCK === 'true'
+
 @Module({
-  imports: [MainModule, CaseModule, AdvertTypeModule],
-  controllers: [JournalController, AdvertTypeController],
+  imports: [
+    SequelizeModule.forFeature([
+      AdvertModel,
+      AdvertMainCategoryModel,
+      AdvertDepartmentModel,
+      AdvertInvolvedPartyModel,
+      AdvertCategoryModel,
+      AdvertCategoriesModel,
+      AdvertCategoryCategoriesModel,
+      AdvertStatusModel,
+    ]),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: IJournalService,
+      useClass: MOCK_DATA ? MockJournalService : JournalService,
+    },
+  ],
+  exports: [IJournalService],
 })
 export class JournalModule {}
