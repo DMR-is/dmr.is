b1e963fd441b04264b9742dd234a7be348b61301
Þórður Hafliðason <tolleinn@gmail.com>
2025-04-02 12:14:07 +0000

    Update users table handling. Remove redundant middleware

--- libs/shared/api-client/src/lib/createEnhancedFetch.ts
+++ libs/shared/api-client/src/lib/createEnhancedFetch.ts
@@ -1,13 +1,11 @@
 import nodeFetch from 'node-fetch'
 
 import { buildFetch } from './buildFetch'
-import { createAuthErrorMiddleware } from './handleAuthErrors'
 import { EnhancedFetchAPI } from './types'
 
 export const createEnhancedFetch = (): EnhancedFetchAPI => {
   const builder = buildFetch(nodeFetch)
 
-  builder.wrap(createAuthErrorMiddleware, {})
   // add more middleware here if needed ...
 
   return builder.getFetch()
