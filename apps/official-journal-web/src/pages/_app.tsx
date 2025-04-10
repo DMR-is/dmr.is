d7db20e8ea3abdc2e29045b9dd7086a655627939
Þórður Hafliðason <tolleinn@gmail.com>
2025-04-10 16:29:14 +0000

    Update refetchInterval

--- apps/official-journal-web/src/pages/_app.tsx
+++ apps/official-journal-web/src/pages/_app.tsx
@@ -41,7 +41,8 @@ export default function App({
   return (
     <SessionProvider
       session={session}
-      refetchInterval={3 * 60}
+      refetchInterval={2 * 60}
+      refetchOnWindowFocus={true}
       basePath="/api/auth"
     >
       <Layout {...layout}>
