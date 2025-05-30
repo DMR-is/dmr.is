c93dcd8a0e81f8ea680d85cef67eab927b5d142b
Kristinn Þorri Þrastarson <kristinn.thorri@gmail.com>
2025-05-30 13:04:28 +0000

    Fixing params statuses

--- apps/official-journal-web/src/components/tabs/CaseOverviewTabs.tsx
+++ apps/official-journal-web/src/components/tabs/CaseOverviewTabs.tsx
@@ -1,5 +1,6 @@
 import { useFilters } from '@dmr.is/ui/hooks/useFilters'
 
+import { CaseStatusEnum } from '../../gen/fetch'
 import { useCases } from '../../hooks/api'
 import { CaseTableRitstjorn } from '../tables/CaseTableRitstjorn'
 
@@ -8,8 +9,20 @@ export const CaseOverviewTabs = () => {
     initialPageSize: 50,
   })
 
+  const statuses = [
+    CaseStatusEnum.Innsent,
+    CaseStatusEnum.Grunnvinnsla,
+    CaseStatusEnum.Yfirlestur,
+    CaseStatusEnum.Tilbúið,
+  ]
+
+  const paramsWithStatuses = {
+    ...params,
+    status: params.status.length > 0 ? params.status : statuses,
+  }
+
   const { data } = useCases({
-    params: params,
+    params: paramsWithStatuses,
   })
 
   return <CaseTableRitstjorn cases={data?.cases} paging={data?.paging} />
