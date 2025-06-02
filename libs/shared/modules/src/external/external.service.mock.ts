f7927c6e4eb87b92ee795825794ea1b8862362ae
Tryggvi Geir Magnússon <tryggvi@hugsmidjan.is>
2025-06-02 11:45:06 +0000

    feat: external service calling reglugerdir when reglugerd is published (#529)
    * external service calling reglugerdir when reglugerd is published
    
    * merge ff
    
    * lint
    
    * fix specs
    
    * casing

---
+++ libs/shared/modules/src/external/external.service.mock.ts
@@ -0,0 +1,17 @@
+import { Logger } from 'winston'
+
+import { Inject } from '@nestjs/common'
+
+import { LOGGER_PROVIDER } from '@dmr.is/logging'
+import { ResultWrapper } from '@dmr.is/types'
+
+import { CaseModel } from '../case/models'
+
+export class ExternalServiceMock {
+  constructor(@Inject(LOGGER_PROVIDER) private readonly logger: Logger) {
+    this.logger.info('Using CaseServiceMock')
+  }
+  publishRegulation(publishedCase: CaseModel): Promise<ResultWrapper> {
+    throw new Error('Method not implemented.')
+  }
+}
