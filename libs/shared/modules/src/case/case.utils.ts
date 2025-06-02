9b8b8af189fd701b9cb007e26a1a3cfecfa75cb9
Kristinn Þorri <kristinn.thorri@gmail.com>
2025-06-02 09:46:36 +0000

    chore: adding typecheck and adding to workflow (#535)
    * chore: adding typecheck and adding to workflow
    
    * adding typecheck to build command
    
    * Adding set shas
    
    * Codegen needs to happen before tsc

--- libs/shared/modules/src/case/case.utils.ts
+++ libs/shared/modules/src/case/case.utils.ts
@@ -1,13 +1,7 @@
-import { CaseOverviewTotalItems, CaseStatusEnum } from '@dmr.is/shared/dto'
-
-import { CaseModel } from './models'
+import { CaseStatusEnum } from '@dmr.is/shared/dto'
 
 type StatusResMapper = {
   [key: string]: string
-}
-
-interface Result {
-  [key: string]: number
 }
 
 export const statusResMapper: StatusResMapper = {
@@ -15,23 +9,4 @@ export const statusResMapper: StatusResMapper = {
   [CaseStatusEnum.Submitted]: 'submitted',
   [CaseStatusEnum.InReview]: 'inReview',
   [CaseStatusEnum.InProgress]: 'inProgress',
-}
-
-export const counterResult = (counter: CaseModel[]) => {
-  const result: Result = counter.reduce((acc: Result, item) => {
-    const { count, caseStatusTitle } = item.dataValues
-
-    const mappedStatus = statusResMapper[caseStatusTitle]
-    if (mappedStatus) {
-      acc[mappedStatus] = (acc[mappedStatus] || 0) + parseInt(count, 10)
-    }
-    return acc
-  }, {})
-
-  return {
-    submitted: result.submitted ?? 0,
-    inProgress: result.inProgress ?? 0,
-    inReview: result.inReview ?? 0,
-    ready: result.ready ?? 0,
-  } as CaseOverviewTotalItems
 }
