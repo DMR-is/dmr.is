f27d072d4d6f29a681e1fa88c0455cfd7e4979c5
Kristinn Þorri Þrastarson <kristinn.thorri@gmail.com>
2025-06-26 13:22:47 +0000

    fix: adding healthcheck endpoint

---
+++ apps/legal-gazette-web/src/pages/api/health.ts
@@ -0,0 +1,7 @@
+// Health check endpoint
+
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ status: 'ok' })
+}
