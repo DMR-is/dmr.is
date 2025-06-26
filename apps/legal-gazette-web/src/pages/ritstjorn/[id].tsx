7c3f11239c5c6f15bfffeca164815e5377ae84c4
Kristinn Þorri Þrastarson <kristinn.thorri@gmail.com>
2025-06-26 11:14:28 +0000

    Adding uuid validation pipe to case controller

--- apps/legal-gazette-web/src/pages/ritstjorn/[id].tsx
+++ apps/legal-gazette-web/src/pages/ritstjorn/[id].tsx
@@ -114,6 +114,10 @@ export const getServerSideProps: GetServerSideProps = async ({
     id: Array.isArray(params.id) ? params.id[0] : params.id,
   })
 
+  if (!initalCase) {
+    return { notFound: true }
+  }
+
   if (!query.tab) {
     const advertId = initalCase.adverts[0]?.id
     return {
