ca3b1184a3d10415b0488e6f54d9b2c1fae3ff9e
Þórður Hafliðason <tolleinn@gmail.com>
2025-07-09 14:46:22 +0000

    cleanup

--- apps/official-journal-api/migrations/m-d20250709-add-legacy-pdf-column.js
+++ apps/official-journal-api/migrations/m-d20250709-add-legacy-pdf-column.js
@@ -3,12 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
     await queryInterface.addColumn('advert', 'document_pdf_legacy', {
       type: Sequelize.TEXT,
       allowNull: true,
@@ -16,12 +10,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
     await queryInterface.removeColumn('advert', 'document_pdf_legacy')
   },
 }
