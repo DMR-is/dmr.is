6a3113c612ab88bdc6cabe3d923366d580434a94
Þórður Hafliðason <tolleinn@gmail.com>
2025-07-09 14:45:31 +0000

    Add legacy pdf column

---
+++ apps/official-journal-api/migrations/m-d20250709-add-legacy-pdf-column.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('advert', 'document_pdf_legacy', {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('advert', 'document_pdf_legacy')
+  },
+}
