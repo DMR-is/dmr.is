7140607dc6b31e4facb23e612d3a2000f44caeb9
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-07-10 09:46:40 +0000

    feat(legal-gazette): Application web (#575)
    * Bootstrapped application web
    
    * Removed unused code
    
    * fonts
    
    * Structuring layout for application web

---
+++ apps/legal-gazette-application-web/src/lib/constants.ts
@@ -0,0 +1,7 @@
+export enum PageRoutes {
+  FRONTPAGE = '/',
+  APPLICATIONS = '/umsoknir',
+  APPLICATION_THROTABU = '/umsoknir/innkollun-throtabus',
+  APPLICATION_DANARBU = '/umsoknir/innkollun-danarbus',
+  ISLAND_IS_COMMONA_APPLICATION = 'https://island.is/umsoknir/logbirtingarblad',
+}
