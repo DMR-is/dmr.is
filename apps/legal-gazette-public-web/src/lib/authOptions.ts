9438ac7c5ecc91ad99680b5deec2fb38fbfa7de8
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-09-24 14:40:05 +0000

    fix(legal-gazette): Revert ids config changes (#670)
    * removing hardcoded redirect_uri
    
    * Remove delegation from legal-gazette-web

--- apps/legal-gazette-public-web/src/lib/authOptions.ts
+++ apps/legal-gazette-public-web/src/lib/authOptions.ts
@@ -153,7 +153,6 @@ export const authOptions: AuthOptions = {
       issuer: `https://${process.env.IDENTITY_SERVER_DOMAIN}`,
       authorization: {
         params: {
-          redirect_uri: `${process.env.IDENTITY_SERVER_LOGOUT_URL}/api/auth/callback/identity-server`,
           scope: `${identityServerConfig.scope}`,
           domain: `https://${process.env.IDENTITY_SERVER_DOMAIN}`,
           protection: 'pkce',
