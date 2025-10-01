1c077bda6a6b985d1820dc954ce20227ff98c0c0
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-10-01 08:44:16 +0000

    Removed unused files

--- apps/legal-gazette-api/src/openapi/external.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentBuilder } from '@nestjs/swagger'
-
-export const externalOpenApi = new DocumentBuilder()
-  .setTitle('The Legal Gazette of Iceland External API')
-  .setDescription('The External API for the Legal Gazette of Iceland')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build()
--- apps/legal-gazette-api/src/openapi/public.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentBuilder } from '@nestjs/swagger'
-
-export const publicOpenApi = new DocumentBuilder()
-  .setTitle('The Legal Gazette of Iceland Public API')
-  .setDescription('The Public API for the Legal Gazette of Iceland')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build()
