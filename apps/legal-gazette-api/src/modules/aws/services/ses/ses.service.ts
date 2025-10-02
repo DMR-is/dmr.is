0c9c3d6bce0dcdabde4b51a94b8f4fcd2546d106
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-10-02 13:10:42 +0000

    Removed the implmentation of the aws ses.

--- apps/legal-gazette-api/src/modules/aws/services/ses/ses.service.ts
+++ apps/legal-gazette-api/src/modules/aws/services/ses/ses.service.ts
@@ -1,35 +1,35 @@
-import nodemailer, { SentMessageInfo } from 'nodemailer'
+import nodemailer from 'nodemailer'
 import Mail from 'nodemailer/lib/mailer'
 
 import { Injectable } from '@nestjs/common'
 
 import { ISESService } from './ses.service.interface'
 
-import { SendRawEmailCommand, SESClient } from '@aws-sdk/client-ses'
-import { fromIni } from '@aws-sdk/credential-providers'
-
 @Injectable()
 export class SESService implements ISESService {
-  private readonly sesClient = new SESClient({
-    region: process.env.AWS_REGION ?? 'eu-west-1',
-    credentials: process.env.AWS_CREDENTIALS_SOURCE
-      ? fromIni({ profile: process.env.AWS_CREDENTIALS_SOURCE })
-      : undefined,
-  })
-
-  private readonly transporter = nodemailer.createTransport({
-    SES: { ses: this.sesClient, aws: { SendRawEmailCommand } },
-  })
-
-  constructor() {
-    if (!this.sesClient) {
-      throw new Error(
-        'Failed to create SES client, check your AWS environment variables',
-      )
-    }
+  sendMail(message: Mail.Options): Promise<nodemailer.SentMessageInfo> {
+    throw new Error('Method not implemented.')
   }
+  // private readonly sesClient = new SESClient({
+  //   region: process.env.AWS_REGION ?? 'eu-west-1',
+  //   credentials: process.env.AWS_CREDENTIALS_SOURCE
+  //     ? fromIni({ profile: process.env.AWS_CREDENTIALS_SOURCE })
+  //     : undefined,
+  // })
 
-  async sendMail(message: Mail.Options): Promise<SentMessageInfo> {
-    return await this.transporter.sendMail(message)
-  }
+  // private readonly transporter = nodemailer.createTransport({
+  //   SES: { ses: this.sesClient, aws: { SendRawEmailCommand } },
+  // })
+
+  // constructor() {
+  //   if (!this.sesClient) {
+  //     throw new Error(
+  //       'Failed to create SES client, check your AWS environment variables',
+  //     )
+  //   }
+  // }
+
+  // async sendMail(message: Mail.Options): Promise<SentMessageInfo> {
+  //   return await this.transporter.sendMail(message)
+  // }
 }
