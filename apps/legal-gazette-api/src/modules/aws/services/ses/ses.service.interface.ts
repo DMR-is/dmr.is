fc281d546041b8aeeb94dbd5c5b534cfcae00b19
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-10-02 13:21:45 +0000

    fix(legal-gazette): Removed the implmentation of the aws ses. (#685)
    * Removed the implmentation of the aws ses.
    
    * Log out instead of throwing
    
    * Fixed typings

--- apps/legal-gazette-api/src/modules/aws/services/ses/ses.service.interface.ts
+++ apps/legal-gazette-api/src/modules/aws/services/ses/ses.service.interface.ts
@@ -2,7 +2,7 @@ import { SentMessageInfo } from 'nodemailer'
 import Mail from 'nodemailer/lib/mailer'
 
 export interface ISESService {
-  sendMail(message: Mail.Options): Promise<SentMessageInfo>
+  sendMail(message: Mail.Options): Promise<void>
 }
 
 export const ISESService = Symbol('ISESService')
--- apps/legal-gazette-api/src/modules/aws/services/ses/ses.service.ts
+++ apps/legal-gazette-api/src/modules/aws/services/ses/ses.service.ts
@@ -1,35 +1,39 @@
-import nodemailer, { SentMessageInfo } from 'nodemailer'
+import nodemailer from 'nodemailer'
 import Mail from 'nodemailer/lib/mailer'
 
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
+
+import { Logger, LOGGER_PROVIDER } from '@dmr.is/logging'
 
 import { ISESService } from './ses.service.interface'
 
-import { SendRawEmailCommand, SESClient } from '@aws-sdk/client-ses'
-import { fromIni } from '@aws-sdk/credential-providers'
-
 @Injectable()
 export class SESService implements ISESService {
-  private readonly sesClient = new SESClient({
-    region: process.env.AWS_REGION ?? 'eu-west-1',
-    credentials: process.env.AWS_CREDENTIALS_SOURCE
-      ? fromIni({ profile: process.env.AWS_CREDENTIALS_SOURCE })
-      : undefined,
-  })
+  constructor(@Inject(LOGGER_PROVIDER) private logger: Logger) {}
 
-  private readonly transporter = nodemailer.createTransport({
-    SES: { ses: this.sesClient, aws: { SendRawEmailCommand } },
-  })
-
-  constructor() {
-    if (!this.sesClient) {
-      throw new Error(
-        'Failed to create SES client, check your AWS environment variables',
-      )
-    }
+  async sendMail(message: Mail.Options): Promise<void> {
+    this.logger.warn('SESService.sendMail is not implemented')
   }
+  // private readonly sesClient = new SESClient({
+  //   region: process.env.AWS_REGION ?? 'eu-west-1',
+  //   credentials: process.env.AWS_CREDENTIALS_SOURCE
+  //     ? fromIni({ profile: process.env.AWS_CREDENTIALS_SOURCE })
+  //     : undefined,
+  // })
 
-  async sendMail(message: Mail.Options): Promise<SentMessageInfo> {
-    return await this.transporter.sendMail(message)
-  }
+  // private readonly transporter = nodemailer.createTransport({
+  //   SES: { ses: this.sesClient, aws: { SendRawEmailCommand } },
+  // })
+
+  // constructor() {
+  //   if (!this.sesClient) {
+  //     throw new Error(
+  //       'Failed to create SES client, check your AWS environment variables',
+  //     )
+  //   }
+  // }
+
+  // async sendMail(message: Mail.Options): Promise<SentMessageInfo> {
+  //   return await this.transporter.sendMail(message)
+  // }
 }
