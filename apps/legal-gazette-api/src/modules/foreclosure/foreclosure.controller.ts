b306876cdcdc49a4318b9da1d6b64651cd6ce39a
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-10-13 08:57:13 +0000

    add endpoints for creating and deleting foreclosure sales and properties

--- apps/legal-gazette-api/src/modules/foreclosure/foreclosure.controller.ts
+++ apps/legal-gazette-api/src/modules/foreclosure/foreclosure.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Inject, Param } from '@nestjs/common'
+import { Body, Controller, Delete, Inject, Param, Post } from '@nestjs/common'
+import { ApiParam } from '@nestjs/swagger'
 
 import { Get } from '@dmr.is/decorators'
 
 import { LGResponse } from '../../decorators/lg-response.decorator'
-import { ForeclosureDto, GetForeclosuresDto } from './dto/foreclosure.dto'
+import {
+  CreateForeclosureSaleDto,
+  ForeclosureDto,
+  ForeclosurePropertyDto,
+} from './dto/foreclosure.dto'
 import { IForeclosureService } from './foreclosure.service.interface'
 
 @Controller({
@@ -16,15 +21,60 @@ export class ForeclosureController {
     private readonly foreclosureService: IForeclosureService,
   ) {}
 
-  @Get()
-  @LGResponse({ operationId: 'getForeclosures', type: GetForeclosuresDto })
-  async getForeclosures(): Promise<GetForeclosuresDto> {
-    return this.foreclosureService.getForeclosures()
-  }
-
   @Get(':id')
-  @LGResponse({ operationId: 'getForeclosureById', type: ForeclosureDto })
+  @LGResponse({
+    operationId: 'getForeclosureById',
+    type: ForeclosureDto,
+    description: 'Get foreclosure by id',
+  })
   async getForeclosureById(@Param('id') id: string): Promise<ForeclosureDto> {
     return this.foreclosureService.getForeclosureById(id)
+  }
+
+  @Post('/sale')
+  @LGResponse({
+    operationId: 'createForeclosureSale',
+    type: 'void',
+    description:
+      'Creates a new foreclosure advertisement along with its properties',
+  })
+  async createForeclosureSale(
+    @Body() body: CreateForeclosureSaleDto,
+  ): Promise<ForeclosureDto> {
+    return this.foreclosureService.createForeclosureSale(body)
+  }
+
+  @Post('/sale/:id/property')
+  @LGResponse({
+    operationId: 'createForeclosureProperty',
+    type: 'void',
+    description: 'Creates a new property for an existing foreclosure sale',
+  })
+  @ApiParam({ name: 'id', description: 'The ID of the foreclosure sale' })
+  async createForeclosureProperty(
+    @Param('id') id: string,
+    @Body() body: CreateForeclosureSaleDto['properties'][0],
+  ): Promise<ForeclosurePropertyDto> {
+    return this.foreclosureService.createForeclosureProperty(id, body)
+  }
+
+  @Delete('/sale/:id/property/:propertyNumber')
+  @LGResponse({
+    operationId: 'deletePropertyFromForeclosure',
+    description: 'Deletes a property from an existing foreclosure sale',
+  })
+  @ApiParam({ name: 'id', description: 'The ID of the foreclosure sale' })
+  @ApiParam({
+    name: 'propertyNumber',
+    description: 'The property number of the property to delete (fastanúmer)',
+  })
+  async deletePropertyFromForeclosure(
+    @Param('id') id: string,
+    @Param('propertyNumber') propertyNumber: string,
+  ): Promise<void> {
+    return this.foreclosureService.deletePropertyFromForeclosure(
+      id,
+      propertyNumber,
+    )
   }
 }
