404d5e7cc1313c92b2e3aa872ae805868cc0f473
Tryggvi Geir Magnússon <tryggvi@hugsmidjan.is>
2025-10-16 13:24:36 +0000

    fix(regulations): storeData logging

--- apps/regulations-api/src/utils/misc.ts
+++ apps/regulations-api/src/utils/misc.ts
@@ -114,36 +114,37 @@ const JSONFILE_MAXAGE = 6 * HOUR
 export const storeData = (data: unknown, path: string) => {
   console.info('storing data to', path)
   try {
-    console.info('trying to store data to', path)
-    const dirName = parse(path).dir
+    // Use /tmp for writable storage in containers
+    const safePath = `/tmp/${path}`
+    console.info('trying to store data to', safePath)
+    const dirName = parse(safePath).dir
     if (!fs.existsSync(dirName)) {
       console.info('creating dir', dirName)
-      fs.mkdirSync(dirName)
+      fs.mkdirSync(dirName, { recursive: true })
     }
-    console.info('writing file', path)
-    fs.writeFileSync(path, JSON.stringify(data))
-    console.info('wrote file', path)
+    console.info('writing file', safePath)
+    fs.writeFileSync(safePath, JSON.stringify(data))
+    console.info('wrote file', safePath)
   } catch (err) {
-    // eslint-disable-next-line no-console
     console.info('Failed to store data to', path)
     console.error(err)
   }
 }
 
 export const loadData = <T>(path: string): T | false => {
-  if (!fs.existsSync(path)) {
+  const safePath = `/tmp/${path}`
+  if (!fs.existsSync(safePath)) {
     return false
   }
   try {
-    const lastModified = fs.statSync(path).mtimeMs
+    const lastModified = fs.statSync(safePath).mtimeMs
 
     // return file if it's available and fresh
     if (Date.now() - lastModified <= JSONFILE_MAXAGE) {
-      const data = fs.readFileSync(path, 'utf8')
+      const data = fs.readFileSync(safePath, 'utf8')
       return JSON.parse(data)
     }
   } catch (err) {
-    // eslint-disable-next-line no-console
     console.error(err)
   }
   return false
