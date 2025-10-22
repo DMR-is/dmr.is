f498772c87e21e008a9f6c87533ed06f045680e3
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-10-22 10:13:48 +0000

    Adding in progress status seed and updating StatusIdEnum

--- apps/legal-gazette-api/src/modules/status/status.model.ts
+++ apps/legal-gazette-api/src/modules/status/status.model.ts
@@ -7,15 +7,14 @@ import { AdvertModel } from '../advert/advert.model'
 import { StatusDto } from './dto/status.dto'
 
 export enum StatusIdEnum {
-  DRAFT = '63726fce-edcf-412d-85e8-087e99c592f2',
   SUBMITTED = 'cd3bf301-52a1-493e-8c80-a391c310c840',
+  IN_PROGRESS = '7ef679c4-4f66-4892-b6ad-ee05e0be4359',
   READY_FOR_PUBLICATION = 'a2f3b1c4-2d5e-4a7b-8c6f-9d1e0f3a2b8c',
   PUBLISHED = 'bd835a1d-0ecb-4aa4-9910-b5e60c30dced',
   REJECTED = 'f3a0b1c4-2d5e-4a7b-8c6f-9d1e0f3a2b8c',
   WITHDRAWN = 'e2f3b1c4-2d5e-4a7b-8c6f-9d1e0f3a2b8c',
 }
 export enum StatusEnum {
-  DRAFT = 'Drög',
   SUBMITTED = 'Innsent',
   READY_FOR_PUBLICATION = 'Tilbúið til útgáfu',
   PUBLISHED = 'Útgefið',
