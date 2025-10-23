91cf74fde7f659b2c1421058fd736bd408c48a90
Kristinn Þorri Þrastarson <kristinn.thorri@gmail.com>
2025-10-23 15:53:41 +0000

    Moving session provider to root

--- apps/legal-gazette-web/src/app/(protected)/layout.tsx
+++ apps/legal-gazette-web/src/app/(protected)/layout.tsx
@@ -1,10 +1,6 @@
-import { redirect } from 'next/navigation'
-import { getServerSession } from 'next-auth'
-
 import { Header } from '@dmr.is/ui/components/Header/Header'
 
 import { Providers } from '../../components/providers/Providers'
-import { authOptions } from '../../lib/auth/authOptions'
 import { TRPCReactProvider } from '../../lib/nTrpc/client/Provider'
 import { HydrateClient, prefetch, trpc } from '../../lib/nTrpc/client/server'
 
@@ -13,18 +9,12 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const session = await getServerSession(authOptions)
-
-  if (session?.invalid) {
-    redirect('/innskraning')
-  }
-
   prefetch(trpc.getAllEntities.queryOptions())
 
   return (
     <TRPCReactProvider>
       <HydrateClient>
-        <Providers session={session}>
+        <Providers>
           <Header variant="blue" />
           {children}
         </Providers>
--- apps/legal-gazette-web/src/components/providers/Providers.tsx
+++ apps/legal-gazette-web/src/components/providers/Providers.tsx
@@ -1,8 +1,5 @@
 'use client'
 
-import { Session } from 'next-auth'
-import { SessionProvider } from 'next-auth/react'
-
 import { NuqsAdapter } from 'nuqs/adapters/next'
 import { IntlProvider } from 'react-intl'
 
@@ -14,42 +11,29 @@ import { flattenMessages } from '../../lib/utils'
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-export const Providers = ({
-  children,
-  session,
-}: {
-  children: React.ReactNode
-  session: Session | null
-}) => {
+export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <NuqsAdapter>
-        <SessionProvider
-          session={session}
-          refetchInterval={1 * 60}
-          refetchOnWindowFocus={true}
-          basePath="/api/auth"
-        >
-          <IntlProvider
-            locale="is"
-            defaultLocale="is"
-            messages={flattenMessages(allMessages)}
-            onError={(err) => {
-              // Chrome only ships with 'en' formatters for NumberFormat and DateTimeFormat.
-              // Ignore these errors since we're not using these formatters.
-              // Bundling polyfills for 'is' significantly increases bundle size and provides no gain.
-              // See: https://app.asana.com/0/1202453499137756/1204509391926816
-              if (err.code === 'MISSING_DATA') {
-                return null
-              }
+        <IntlProvider
+          locale="is"
+          defaultLocale="is"
+          messages={flattenMessages(allMessages)}
+          onError={(err) => {
+            // Chrome only ships with 'en' formatters for NumberFormat and DateTimeFormat.
+            // Ignore these errors since we're not using these formatters.
+            // Bundling polyfills for 'is' significantly increases bundle size and provides no gain.
+            // See: https://app.asana.com/0/1202453499137756/1204509391926816
+            if (err.code === 'MISSING_DATA') {
+              return null
+            }
 
-              // eslint-disable-next-line no-console
-              console.error('Error in IntlProvider', { exception: err })
-            }}
-          >
-            <FilterProvider>{children}</FilterProvider>
-          </IntlProvider>
-        </SessionProvider>
+            // eslint-disable-next-line no-console
+            console.error('Error in IntlProvider', { exception: err })
+          }}
+        >
+          <FilterProvider>{children}</FilterProvider>
+        </IntlProvider>
       </NuqsAdapter>
       <ToastContainer closeButton={true} timeout={2000} />
       <ReactQueryDevtools />
