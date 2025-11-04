3fa4dd883fad7a46e7ddbb44d444032c76d426df
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-11-04 11:22:39 +0000

    Fetching the statistics

--- apps/legal-gazette-web/src/app/(protected)/page.tsx
+++ apps/legal-gazette-web/src/app/(protected)/page.tsx
@@ -1,19 +1,26 @@
-import dynamic from 'next/dynamic'
-
 import { ApplicationContainer } from '../../components/front-page/ApplicationContainer'
 import { HeroContainer } from '../../components/front-page/HeroContainer'
-
-const SectionContainer = dynamic(
-  () =>
-    import('../../components/front-page/SectionContainer').then(
-      (mod) => mod.SectionContainer,
-    ),
-  {
-    ssr: false,
-  },
-)
+import { SectionContainer } from '../../components/front-page/SectionContainer'
+import { fetchQuery, trpc } from '../../lib/nTrpc/client/server'
 
 export default async function IndexPage() {
+  const countByStatusPromise = fetchQuery(
+    trpc.getCountByStatuses.queryOptions(),
+  )
+  const advertsInprogressStats = fetchQuery(
+    trpc.getAdvertsInProgressStats.queryOptions(),
+  )
+  const readyForPublishingStats = fetchQuery(
+    trpc.getAdvertsToBePublishedStats.queryOptions(),
+  )
+
+  const [countByStatus, inprogressStats, readyForPublishing] =
+    await Promise.all([
+      countByStatusPromise,
+      advertsInprogressStats,
+      readyForPublishingStats,
+    ])
+
   return (
     <>
       <HeroContainer />
