fd8acd20d6d05f99c2985ce17971dbb39a5b972e
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-11-07 13:33:58 +0000

    Updating island-is application module

--- apps/legal-gazette-api/src/modules/applications/controllers/island-is-application.controller.ts
+++ apps/legal-gazette-api/src/modules/applications/controllers/island-is-application.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common'
+import { ApiBearerAuth } from '@nestjs/swagger'
+
+import { DMRUser } from '@dmr.is/auth/dmrUser'
+import { CurrentUser } from '@dmr.is/decorators'
+import { TokenJwtAuthGuard } from '@dmr.is/modules'
+
+import { LGResponse } from '../../../decorators/lg-response.decorator'
+import { IApplicationService } from '../application.service.interface'
+import { IslandIsSubmitCommonApplicationDto } from '../dto/island-is-application.dto'
 
 @Controller({
   path: 'applications/island-is',
   version: '1',
 })
-export class IslandIsApplicationController {}
+@ApiBearerAuth()
+@UseGuards(TokenJwtAuthGuard)
+export class IslandIsApplicationController {
+  constructor(
+    @Inject(IApplicationService)
+    private readonly applicationService: IApplicationService,
+  ) {}
+
+  @Post('submit')
+  @LGResponse({ operationId: 'submitIslandIsApplication', status: 201 })
+  async submitApplication(
+    @Body() body: IslandIsSubmitCommonApplicationDto,
+    @CurrentUser() user: DMRUser,
+  ) {
+    return this.applicationService.submitIslandIsApplication(body, user)
+  }
+}
