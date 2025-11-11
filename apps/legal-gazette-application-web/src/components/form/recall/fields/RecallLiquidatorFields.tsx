99888f7296b7d087aa4d5ea5413b5a5241a66b0a
Þórður H <tolleinn@gmail.com>
2025-11-11 13:13:06 +0000

    fix(ojoi): update custom location on base update (#782)
    Update updateLiquidatorRecallRequirementStatementLocation

--- apps/legal-gazette-application-web/src/components/form/recall/fields/RecallLiquidatorFields.tsx
+++ apps/legal-gazette-application-web/src/components/form/recall/fields/RecallLiquidatorFields.tsx
@@ -1,6 +1,7 @@
 import { useFormContext } from 'react-hook-form'
 
 import {
+  ApplicationRequirementStatementEnum,
   RecallApplicationInputFields,
   RecallApplicationSchema,
 } from '@dmr.is/legal-gazette/schemas'
@@ -11,8 +12,11 @@ import { InputController } from '../../controllers/InputController'
 
 export const RecallLiquidatorFields = () => {
   const { getValues } = useFormContext<RecallApplicationSchema>()
-  const { updateLiquidatorName, updateLiquidatorLocation } =
-    useUpdateRecallApplication(getValues('metadata.applicationId'))
+  const {
+    updateLiquidatorName,
+    updateLiquidatorLocation,
+    updateLiquidatorRecallRequirementStatementLocation,
+  } = useUpdateRecallApplication(getValues('metadata.applicationId'))
 
   return (
     <GridRow rowGap={[2, 3]}>
@@ -31,7 +35,16 @@ export const RecallLiquidatorFields = () => {
         <InputController
           label="Staðsetning skiptastjóra"
           name={RecallApplicationInputFields.LIQUIDATOR_LOCATION}
-          onBlur={(val) => updateLiquidatorLocation(val)}
+          onBlur={(val) => {
+            updateLiquidatorLocation(val)
+            if (
+              getValues(
+                RecallApplicationInputFields.RECALL_REQUIREMENT_STATEMENT_TYPE,
+              ) === ApplicationRequirementStatementEnum.LIQUIDATORLOCATION
+            ) {
+              updateLiquidatorRecallRequirementStatementLocation(val)
+            }
+          }}
           required
         />
       </GridColumn>
--- apps/legal-gazette-application-web/src/components/form/recall/fields/RecallRequirementStatementFields.tsx
+++ apps/legal-gazette-application-web/src/components/form/recall/fields/RecallRequirementStatementFields.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useFormContext } from 'react-hook-form'
 
 import {
