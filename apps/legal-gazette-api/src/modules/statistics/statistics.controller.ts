8bf5caf552fca7752182a5178fd1db489840742a
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-11-13 09:26:19 +0000

    Moved statistics dto the the dto dir

--- apps/legal-gazette-api/src/modules/statistics/statistics.controller.ts
+++ apps/legal-gazette-api/src/modules/statistics/statistics.controller.ts
@@ -8,7 +8,7 @@ import {
   GetAdvertsInProgressStatsDto,
   GetAdvertsToBePublishedStatsDto,
   GetCountByStatusesDto,
-} from './dto/statistics.dto'
+} from '../../dto/statistics.dto'
 import { IStatisticsService } from './statistics.service.interface'
 
 @Controller({
