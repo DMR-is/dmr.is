9fcdccc8acb73a6a8c1ca0d4b6957783ba27a6d1
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-11-13 11:13:34 +0000

    Moved subscriber dto to the model

--- apps/legal-gazette-api/src/modules/subscribers/subscriber.controller.ts
+++ apps/legal-gazette-api/src/modules/subscribers/subscriber.controller.ts
@@ -5,7 +5,7 @@ import { CurrentUser } from '@dmr.is/decorators'
 import { TokenJwtAuthGuard } from '@dmr.is/modules'
 
 import { LGResponse } from '../../decorators/lg-response.decorator'
-import { SubscriberDto } from './dto/subscriber.dto'
+import { SubscriberDto } from '../../models/subscriber.model'
 import { ISubscriberService } from './subscriber.service.interface'
 
 @ApiBearerAuth()
