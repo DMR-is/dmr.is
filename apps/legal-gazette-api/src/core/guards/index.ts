c92b6956d35234d78d8fef927a0c35e687a3d9ca
Kristinn Þorri Þrastarson <kristinn.thorri@gmail.com>
2025-12-01 12:44:33 +0000

    Removing unused export and updating clientConfig

--- apps/legal-gazette-api/src/core/guards/index.ts
+++ apps/legal-gazette-api/src/core/guards/index.ts
@@ -1,4 +1,3 @@
-export { AdminGuard } from './admin.guard'
 export { AuthorizationGuard } from './authorization.guard'
 export { CurrentNationalRegistryPersonGuard } from './current-submitte.guard'
 export { MachineClientGuard } from './machine-client.guard'
