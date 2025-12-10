4704122a5e6e113912a0a9789a2e0e6d39ab233f
Ulfur Einarsson <ulfur@hugsmidjan.is>
2025-12-10 10:58:04 +0000

    fix: remove lorem ipsum application web + update clientConfig

--- apps/legal-gazette-application-web/src/components/form/recall/RecallForm.tsx
+++ apps/legal-gazette-application-web/src/components/form/recall/RecallForm.tsx
@@ -93,12 +93,6 @@ export const RecallForm = (props: RecallFormProps) => {
                   </Button>
                 </LinkV2>
               </Inline>
-              <Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </Text>
             </Stack>
             <RecallAdvertFields />
             <RecallSettlementFields />
