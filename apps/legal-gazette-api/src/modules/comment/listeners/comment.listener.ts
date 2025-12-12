849ee34fe953a659b60ca885a77bd03c54922285
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-12-12 09:34:46 +0000

    feat(legal-gazette): Streamline event emitting for the api (#874)
    Using transaction when emitting events to use the afterCommit hook to only emit the events when the function sucessfully runs.

--- apps/legal-gazette-api/src/modules/comment/listeners/comment.listener.ts
+++ apps/legal-gazette-api/src/modules/comment/listeners/comment.listener.ts
@@ -1,5 +1,3 @@
-import { Sequelize } from 'sequelize-typescript'
-
 import { Inject, Injectable } from '@nestjs/common'
 import { OnEvent } from '@nestjs/event-emitter'
 
@@ -17,65 +15,65 @@ export class CommentListener {
   constructor(
     @Inject(LOGGER_PROVIDER) private readonly logger: Logger,
     @Inject(ICommentService) private readonly commentService: ICommentService,
-    private sequelize: Sequelize,
   ) {}
 
-  @OnEvent(LegalGazetteEvents.ADVERT_CREATED, { async: true })
+  @OnEvent(LegalGazetteEvents.ADVERT_CREATED)
   async handleAdvertCreatedEvent(payload: CreateSubmitCommentEvent) {
-    // we gotta make a new transaction context for CLS to work
-    await this.sequelize.transaction(async (_t) => {
-      this.logger.info('Handling advert.created event', {
-        payload,
+    if (payload.external) {
+      if (!payload.actorId || !payload.actorName) {
+        this.logger.error(
+          'Missing actorId or actorName for external submit comment, skipping comment creation',
+          {
+            advertId: payload.advertId,
+            context: 'CommentListener',
+          },
+        )
+
+        return
+      }
+
+      this.logger.info('Creating submit comment for external system', {
+        advertId: payload.advertId,
         context: 'CommentListener',
       })
-
-      if (payload.external) {
-        if (!payload.actorId || !payload.actorName) {
-          this.logger.error(
-            'Missing actorId or actorName for external submit comment, skipping comment creation',
-            {
-              advertId: payload.advertId,
-              context: 'CommentListener',
-            },
-          )
-
-          return
-        }
-
-        this.logger.info('Creating submit comment for external system', {
-          advertId: payload.advertId,
-          context: 'CommentListener',
-        })
+      try {
         return await this.commentService.createSubmitCommentForExternalSystem(
           payload.advertId,
           payload.actorId,
           payload.actorName,
         )
+      } catch (error) {
+        this.logger.error(
+          'Failed to create submit comment for external system',
+          {
+            advertId: payload.advertId,
+            error,
+            context: 'CommentListener',
+          },
+        )
+        return
       }
+    }
 
-      this.logger.info('Creating submit comment', {
-        advertId: payload.advertId,
-        context: 'CommentListener',
-      })
-      await this.commentService.createSubmitComment(payload.advertId, {
-        actorId: payload.actorId,
-      })
+    this.logger.info('Creating submit comment', {
+      advertId: payload.advertId,
+      context: 'CommentListener',
+    })
+    await this.commentService.createSubmitComment(payload.advertId, {
+      actorId: payload.actorId,
     })
   }
 
-  @OnEvent(LegalGazetteEvents.STATUS_CHANGED, { async: true })
+  @OnEvent(LegalGazetteEvents.STATUS_CHANGED)
   async handleStatusChangedEvent(payload: CreateStatusChangeCommentEvent) {
-    // we gotta make a new transaction context for CLS to work
-    await this.sequelize.transaction(async (_t) => {
-      this.logger.info('Handling status.changed event', {
-        payload,
-        context: 'CommentListener',
-      })
+    this.logger.info('Handling status.changed event', {
+      payload,
+      context: 'CommentListener',
+    })
 
-      await this.commentService.createStatusUpdateComment(payload.advertId, {
-        actorId: payload.actorId,
-        receiverId: payload.statusId,
-      })
+    await this.commentService.createStatusUpdateComment(payload.advertId, {
+      actorId: payload.actorId,
+      receiverId: payload.statusId,
     })
   }
 }
