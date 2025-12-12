a0a809f6dcc1fda926e9dbe36fcaa1df8341c8bd
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-12-12 14:11:41 +0000

    Model implementation

--- apps/legal-gazette-api/src/models/tbr-company-settings.model.ts
+++ apps/legal-gazette-api/src/models/tbr-company-settings.model.ts
@@ -1,6 +1,10 @@
 import { Column, DataType } from 'sequelize-typescript'
 
+import { ApiProperty } from '@nestjs/swagger'
+
 import { BaseModel } from '@dmr.is/shared/models/base'
+
+import { DetailedDto } from '../core/dto/detailed.dto'
 
 export type TBRCompanySettingsAttributes = {
   name: string
@@ -30,4 +34,43 @@ export class TBRCompanySettingsModel extends BaseModel<
 
   @Column({ type: DataType.STRING(50), allowNull: false })
   code!: string
+
+  static fromModel(model: TBRCompanySettingsModel): TBRCompanySettingsItemDto {
+    return {
+      id: model.id,
+      createdAt: model.createdAt.toISOString(),
+      updatedAt: model.updatedAt.toISOString(),
+      name: model.name,
+      nationalId: model.nationalId,
+      email: model.email,
+      phone: model.phone,
+      code: model.code,
+    }
+  }
+
+  fromModel(): TBRCompanySettingsItemDto {
+    return TBRCompanySettingsModel.fromModel(this)
+  }
+}
+
+export class TBRCompanySettingsItemDto extends DetailedDto {
+  @ApiProperty({ type: String })
+  name!: string
+
+  @ApiProperty({ type: String })
+  nationalId!: string
+
+  @ApiProperty({ type: String, required: false })
+  email?: string
+
+  @ApiProperty({ type: String, required: false })
+  phone?: string
+
+  @ApiProperty({ type: String })
+  code!: string
+}
+
+export class TBRCompanySettingsListDto {
+  @ApiProperty({ type: [TBRCompanySettingsItemDto] })
+  items!: TBRCompanySettingsItemDto[]
 }
