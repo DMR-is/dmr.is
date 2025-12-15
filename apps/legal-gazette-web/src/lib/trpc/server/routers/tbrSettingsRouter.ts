27177747ff69d9fef78808090144502983135655
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-12-15 08:50:46 +0000

    Updating the client and creating the trpc router

---
+++ apps/legal-gazette-web/src/lib/trpc/server/routers/tbrSettingsRouter.ts
@@ -0,0 +1,64 @@
+import z from 'zod'
+
+import { protectedProcedure, router } from '../trpc'
+
+export const getTbrSettingsInput = z.object({
+  search: z.string().optional(),
+  activeOnly: z.boolean().optional(),
+  page: z.number().optional(),
+  pageSize: z.number().optional(),
+})
+
+export const createTbrSettingInput = z.object({
+  name: z.string(),
+  nationalId: z.string(),
+  email: z.string().optional(),
+  phone: z.string().optional(),
+})
+
+export const idInput = z.object({
+  id: z.string(),
+})
+
+export const updateTbrSettingInput = createTbrSettingInput
+  .extend(idInput.shape)
+  .partial()
+  .required({ id: true })
+
+export const tbrSettingsRouter = router({
+  getTbrSettings: protectedProcedure
+    .input(getTbrSettingsInput)
+    .query(
+      async ({ ctx, input }) => await ctx.api.getTBRCompanySettings(input),
+    ),
+  createTbrSetting: protectedProcedure.input(createTbrSettingInput).mutation(
+    async ({ ctx, input }) =>
+      await ctx.api.createTBRCompanySettings({
+        createTBRCompanySettingsDto: input,
+      }),
+  ),
+  updateTbrSetting: protectedProcedure
+    .input(updateTbrSettingInput)
+    .mutation(async ({ ctx, input }) => {
+      const { id, ...updateData } = input
+      return await ctx.api.updateTBRCompanySettings({
+        id: id,
+        updateTbrCompanySettingsDto: updateData,
+      })
+    }),
+  deleteTbrSetting: protectedProcedure
+    .input(idInput)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.api.deleteTBRCompanySettings({ id: input.id })
+    }),
+  activateSetting: protectedProcedure
+    .input(idInput)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.api.activateTBRCompanySettings({ id: input.id })
+    }),
+  deactivateSetting: protectedProcedure
+    .input(idInput)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.api.deactivateTBRCompanySettings({ id: input.id })
+    }),
+})
