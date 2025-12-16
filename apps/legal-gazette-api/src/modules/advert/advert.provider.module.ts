281b4a175ea72b974d00ca7eb614dd80c2376e94
Þórður Hafliðason <tolleinn@gmail.com>
2025-12-16 11:13:25 +0000

    Remove unused code

--- apps/legal-gazette-api/src/modules/advert/advert.provider.module.ts
+++ apps/legal-gazette-api/src/modules/advert/advert.provider.module.ts
@@ -7,7 +7,6 @@ import { UserModel } from '../../models/users.model'
 import { TypeCategoriesProviderModule } from '../type-categories/type-categories.provider.module'
 import { PriceCalculatorProviderModule } from './calculator/price-calculator.provider.module'
 import { PdfProviderModule } from './pdf/pdf.provider.module'
-import { PublishingTaskModule } from './tasks/publishing/publishing.task.module'
 import { AdvertService } from './advert.service'
 import { IAdvertService } from './advert.service.interface'
 
