b1cf4df823a35a5971e79afb8f47b5bc080ff044
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2025-12-18 14:33:41 +0000

    Updated refinement for publishing dates. Made the first division meeting required to be atleast 9 weeks after (2months and 1 week) the first publication

--- libs/legal-gazette/schemas/src/lib/base/publishing-dates.ts
+++ libs/legal-gazette/schemas/src/lib/base/publishing-dates.ts
@@ -1,5 +1,20 @@
 import z from 'zod'
 
+const validateFuture = (dates: string[]) => {
+  const now = new Date()
+  now.setHours(0, 0, 0, 0)
+  return dates.every((date) => new Date(date) > now)
+}
+
+const validateOrder = (dates: string[]) => {
+  return dates.every((date, index) => {
+    if (index === 0) return true
+    const previousDate = new Date(dates[index - 1])
+    const currentDate = new Date(date)
+    return currentDate > previousDate
+  })
+}
+
 export const publishingDatesSchema = z.array(z.iso.datetime()).optional()
 
 export const publishingDatesSchemaRefined = z
@@ -12,6 +27,12 @@ export const publishingDatesSchemaRefined = z
   .max(3, {
     error: 'Hámark þrír birtingardagar mega vera til staðar',
   })
+  .refine(validateFuture, {
+    message: 'Birtingardagar verða að vera í framtíðinni',
+  })
+  .refine(validateOrder, {
+    message: 'Birtingardagar verða vera í réttri röð frá fyrsta til síðasta',
+  })
 
 export const publishingDatesRecallSchemaRefined = z
   .array(z.iso.datetime(), {
@@ -20,7 +41,21 @@ export const publishingDatesRecallSchemaRefined = z
   .min(2, {
     error: 'Að minnsta kosti tveir birtingardagar verða að vera til staðar',
   })
-  .refine((dates) => dates.length >= 2 && dates.length <= 3, {
-    message:
-      'Að minnsta kosti tveir og mest þrír birtingardagar verða að vera til staðar',
+  .max(3, {
+    error: 'Hámark þrír birtingardagar mega vera til staðar',
+  })
+  .refine(
+    (dates) => {
+      return dates.length >= 2 && dates.length <= 3
+    },
+    {
+      message:
+        'Að minnsta kosti tveir og mest þrír birtingardagar verða að vera til staðar',
+    },
+  )
+  .refine(validateFuture, {
+    message: 'Birtingardagar verða að vera í framtíðinni',
+  })
+  .refine(validateOrder, {
+    message: 'Birtingardagar verða vera í réttri röð frá fyrsta til síðasta',
   })
