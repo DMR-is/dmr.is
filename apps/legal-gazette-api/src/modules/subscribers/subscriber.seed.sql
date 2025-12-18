b01b109660a1880faf7df7a7c67f40b75a091f48
Þórður H <tolleinn@gmail.com>
2025-12-18 11:47:24 +0000

    Fix SQL insert statement formatting

--- apps/legal-gazette-api/src/modules/subscribers/subscriber.seed.sql
+++ apps/legal-gazette-api/src/modules/subscribers/subscriber.seed.sql
@@ -1,16 +1,10 @@
 INSERT INTO
-  LEGAL_GAZETTE_SUBSCRIBERS (
-    id,
-    national_id,
-    is_active,
-    subscribed_from,
-    subscribed_to
-  )
+  LEGAL_GAZETTE_SUBSCRIBERS (id, national_id, is_active, subscribed_from, subscribed_to)
 VALUES
   (
     'b4e98cee-a4d8-4924-90df-b820c4bc0801',
     '0101302399',
     true,
-    '2025-12-01 00:00:00+00',
-    '2026-12-01 00:00:00+00'
+    '2025-12-01-00:00:00+00',
+    '2026-12-01-00:00:00+00'
   );
