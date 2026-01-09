0ea9afd26d4c753981f25cbf0dbf7d560fcfa1bc
Þórður Hafliðason <tolleinn@gmail.com>
2026-01-09 13:05:44 +0000

    Temp revert new piilog

--- libs/logging/src/lib/formatters.ts
+++ libs/logging/src/lib/formatters.ts
@@ -1,16 +1,12 @@
 import { MESSAGE } from 'triple-beam'
 import { format } from 'winston'
 
-import { maskNationalId, maskPiiInObject } from './maskNationalId'
+import { maskNationalId } from './maskNationalId'
 
 const messageSymbol = MESSAGE as unknown as string
 
 export const maskNationalIdFormatter = format((info) => {
   // Mask national IDs in the log message string
   info[messageSymbol] = maskNationalId(info[messageSymbol])
-
-  // Mask PII in metadata objects (nationalId, kennitala, ssn, national_id fields)
-  const maskedInfo = maskPiiInObject(info)
-
-  return maskedInfo
+  return info
 })
