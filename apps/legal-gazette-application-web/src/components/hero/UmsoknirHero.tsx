1de8a2b2cb9615d0168ed01f9e5ce84b5b5dd061
Ulfur Einarsson <ulfur@hugsmidjan.is>
2026-01-16 12:14:40 +0000

    feat(lg): add instructions

--- apps/legal-gazette-application-web/src/components/hero/UmsoknirHero.tsx
+++ apps/legal-gazette-application-web/src/components/hero/UmsoknirHero.tsx
@@ -1,17 +1,35 @@
 'use client'
 
-import { Box } from '@dmr.is/ui/components/island-is'
+import Hero from '@dmr.is/ui/components/Hero/Hero'
+import { Box, LinkV2, Text } from '@dmr.is/ui/components/island-is'
 
 import { CreateApplication } from '../application/CreateApplication'
-import { Hero } from './Hero'
+import { createUrlFromHost } from 'libs/shared/utils/src/lib/clientUtils'
 
 export const UmsoknirHero = () => {
+  const logbirtinUrl = createUrlFromHost(window.location.host, false)
+
   return (
     <Box paddingY={[3, 4, 5, 6]}>
       <Hero
         variant="small"
         title="Lögbirtingablaðið - auglýsingar"
-        description="Hér getur þú valið um hverskonar auglýsingu þú vilt senda inn til Lögbirtingablaðsins."
+        description={
+          <Text variant="intro">
+            Velkomin á nýja síðu fyrir auglýsendur Lögbirtingablaðsins. Hér má
+            sjá <br />
+            <Text color="blue400" fontWeight="medium" as="span" variant="intro">
+              <LinkV2
+                href={logbirtinUrl + '/sidur/leidbeiningar'}
+                underlineVisibility="always"
+                underline="normal"
+              >
+                leiðbeiningar
+              </LinkV2>{' '}
+            </Text>
+            fyrir alla vefi Lögbirtingablaðsins.
+          </Text>
+        }
         contentSpan={['12/12', '8/12']}
         imageSpan={['12/12', '3/12']}
         image={{
