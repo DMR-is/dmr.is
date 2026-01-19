382e755296825a3bd55b62c63a2b64878535adc7
Kristinn Þorri Þrastarson <kristinn.thorri@gmail.com>
2026-01-19 13:00:50 +0000

    fix(lg): lets hard code, because layout fetching seems to be a problem when using runtimevars

--- apps/legal-gazette-public-web/src/app/layout.tsx
+++ apps/legal-gazette-public-web/src/app/layout.tsx
@@ -1,12 +1,10 @@
 import { getServerSession } from 'next-auth'
 
-import { fetchQuery } from '@dmr.is/trpc/client/server'
 import { LGFooter } from '@dmr.is/ui/components/Footer/LGFooter'
 
 import { Providers } from '../components/client-components/providers/Providers'
 import { authOptions } from '../lib/authOptions'
 import ProviderTRPC from '../lib/trpc/client/Provider'
-import { trpc } from '../lib/trpc/client/server'
 
 import '../styles/global.css'
 
@@ -16,7 +14,6 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   const session = await getServerSession(authOptions)
-  const baseUrl = await fetchQuery(trpc.getMyBaseUrl.queryOptions())
 
   return (
     <html lang="is">
@@ -24,7 +21,7 @@ export default async function RootLayout({
         <ProviderTRPC>
           <Providers session={session}>{children}</Providers>
         </ProviderTRPC>
-        <LGFooter baseUrl={baseUrl} site="web" />
+        <LGFooter site="web" />
       </body>
     </html>
   )
