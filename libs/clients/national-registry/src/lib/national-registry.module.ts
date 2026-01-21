faa21463d721a0db9196453af728728e26ff5728
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2026-01-21 09:06:54 +0000

    Changing logger.warning to logger.warn

--- libs/clients/national-registry/src/lib/national-registry.module.ts
+++ libs/clients/national-registry/src/lib/national-registry.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common'
 
-import { LoggingModule } from '@dmr.is/logging'
-
-import { NationalRegistryService } from './national-registry.service'
 import { INationalRegistryService } from './national-registry.service.interface'
 
 @Module({
-  imports: [LoggingModule],
+  imports: [],
   controllers: [],
   providers: [
     {
