23d9e50ef024722e071df04ed9e567664e55f0a2
Kristinn Þorri Þrastarson <kristinn.thorri@gmail.com>
2026-01-22 11:41:21 +0000

    added charge category function that returns correct category based on company lookup

--- apps/legal-gazette-api/src/modules/advert/calculator/price-calculator.service.interface.ts
+++ apps/legal-gazette-api/src/modules/advert/calculator/price-calculator.service.interface.ts
@@ -4,6 +4,8 @@ export interface IPriceCalculatorService {
   getPaymentData(advertId: string): Promise<GetPaymentDataResponseDto>
 
   getEstimatedPriceForApplication(applicationId: string): Promise<number>
+
+  getChargeCategory(nationalId: string): Promise<string>
 }
 
 export const IPriceCalculatorService = Symbol('IPriceCalculatorService')
