78a63f6d0219f0dacec126e1b8a80d8b3be86073
Kristinn Þorri Þrastarson <kristinn.thorri@gmail.com>
2026-02-04 10:58:51 +0000

    fix(lg): Adding cache to provider module

--- apps/legal-gazette-api/src/modules/advert/publications/publication.provider.module.ts
+++ apps/legal-gazette-api/src/modules/advert/publications/publication.provider.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 
+import { createRedisCacheOptions } from '@dmr.is/utils/cache'
+
 import { AdvertModel } from '../../../models/advert.model'
 import { AdvertPublicationModel } from '../../../models/advert-publication.model'
 import { PublicationService } from './publication.service'
 import { IPublicationService } from './publication.service.interface'
 
 @Module({
-  imports: [SequelizeModule.forFeature([AdvertModel, AdvertPublicationModel])],
+  imports: [
+    createRedisCacheOptions('lg-advert-publications'),
+    SequelizeModule.forFeature([AdvertModel, AdvertPublicationModel]),
+  ],
   controllers: [],
   providers: [
     {
