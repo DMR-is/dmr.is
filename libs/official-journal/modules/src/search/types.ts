bbc532237275ab7262389f9753115e253847c72f
Þórður H <tolleinn@gmail.com>
2026-02-09 14:18:41 +0000

    fix(ojoi): Add maintype option to search filter for lean adverts (#1101)
    Add maintype option to search filter for lean adverts

--- libs/official-journal/modules/src/search/types.ts
+++ libs/official-journal/modules/src/search/types.ts
@@ -3,6 +3,11 @@ import { AdvertLean } from '@dmr.is/shared/dto'
 export type SearchAdvertType = AdvertLean & {
   bodyText: string
   caseNumber: string
+  mainType?: {
+    id: string
+    title: string
+    slug: string
+  }
 }
 
 export type UpdateAdvertInIndexRes = { advertId: string; success: boolean }
