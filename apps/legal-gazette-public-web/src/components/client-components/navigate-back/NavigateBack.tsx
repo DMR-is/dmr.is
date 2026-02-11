c094047a9c6fc796ad7d0b3b2bb9d65c5d83499e
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2026-02-11 14:49:30 +0000

    fix(lg): Navigate back button (#1123)
    Fixed the navigate back component, now always return a link with correct param check

--- apps/legal-gazette-public-web/src/components/client-components/navigate-back/NavigateBack.tsx
+++ apps/legal-gazette-public-web/src/components/client-components/navigate-back/NavigateBack.tsx
@@ -1,26 +1,15 @@
 'use client'
 
-import { useParams, useRouter } from 'next/navigation'
+import { useParams } from 'next/navigation'
 
 import { Button, LinkV2 } from '@dmr.is/ui/components/island-is'
 
 export const NavigateBack = () => {
   const params = useParams()
-  const router = useRouter()
 
-  const isDetailPage = params?.id !== undefined
-  const href = isDetailPage ? '/auglysingar' : '/'
+  const href = params?.publicationNumber !== undefined ? '/auglysingar' : '/'
 
-  return isDetailPage ? (
-    <Button
-      onClick={() => router.back()}
-      size="small"
-      variant="text"
-      preTextIcon="arrowBack"
-    >
-      Til baka
-    </Button>
-  ) : (
+  return (
     <LinkV2 href={href}>
       <Button
         as="div"
