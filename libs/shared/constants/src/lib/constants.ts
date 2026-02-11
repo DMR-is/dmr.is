08c4b3ce15a03b40a9e111d03c7460144267471d
Þórður Hafliðason <tolleinn@gmail.com>
2026-02-11 15:19:46 +0000

    Update image handling for pdf generation

--- libs/shared/constants/src/lib/constants.ts
+++ libs/shared/constants/src/lib/constants.ts
@@ -115,3 +115,6 @@ export const REGULATION_TYPES = [
   'b-deild-reglugerd',
   'c-deild-reglugerd',
 ]
+
+export const CHROME_USER_AGENT =
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
