71ec6233936f0e7d95f3c8984373f2886763f98d
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2026-02-20 17:34:23 +0000

    Adding user module as dependency so that the guards can access it.

--- libs/official-journal/modules/src/issues/issues.module.ts
+++ libs/official-journal/modules/src/issues/issues.module.ts
@@ -5,6 +5,8 @@ import { AdvisoryLockModule, AwsModule } from '@dmr.is/shared/modules'
 
 import { AdvertModel } from '../journal/models'
 import { PdfModule } from '../pdf/pdf.module'
+import { UserModule } from '../user/user.module'
+import { IssuesController } from './issues.controller'
 import { IssuesModel } from './issues.model'
 import { IssusesService } from './issues.service'
 import { IIssuesService } from './issues.service.interface'
@@ -14,8 +16,10 @@ import { IIssuesService } from './issues.service.interface'
     AdvisoryLockModule,
     PdfModule,
     AwsModule,
+    UserModule,
     SequelizeModule.forFeature([AdvertModel, IssuesModel]),
   ],
+  controllers: [IssuesController],
   providers: [
     {
       provide: IIssuesService,
