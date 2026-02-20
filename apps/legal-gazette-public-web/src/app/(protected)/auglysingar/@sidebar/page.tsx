8258140336cfd50d50c1a7292920ef0b2f09c1e0
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2026-02-20 09:27:57 +0000

    Update the navigate back to preserve the search params on the previous page.

--- apps/legal-gazette-public-web/src/app/(protected)/auglysingar/@sidebar/page.tsx
+++ apps/legal-gazette-public-web/src/app/(protected)/auglysingar/@sidebar/page.tsx
@@ -1,5 +1,13 @@
+import { Stack } from '@dmr.is/ui/components/island-is/Stack'
+
+import { NavigateBack } from '../../../../components/client-components/navigate-back/NavigateBack'
 import { SearchSidebar } from '../../../../components/client-components/search-page/sidebar/Sidebar'
 
 export default function SearchPageSidebar() {
-  return <SearchSidebar />
+  return (
+    <Stack space={[1, 2]}>
+      <NavigateBack url="/" />
+      <SearchSidebar />
+    </Stack>
+  )
 }
--- apps/legal-gazette-public-web/src/app/(protected)/auglysingar/layout.tsx
+++ apps/legal-gazette-public-web/src/app/(protected)/auglysingar/layout.tsx
@@ -2,7 +2,6 @@ import { Box } from '@dmr.is/ui/components/island-is/Box'
 import { GridContainer } from '@dmr.is/ui/components/island-is/GridContainer'
 import { Stack } from '@dmr.is/ui/components/island-is/Stack'
 
-import { NavigateBack } from '../../../components/client-components/navigate-back/NavigateBack'
 import * as styles from './grid-layout.css'
 
 export default async function Layout({
@@ -18,12 +17,7 @@ export default async function Layout({
     <>
       <GridContainer className="print-hidden">
         <Box paddingY={8} className={styles.gridLayout}>
-          <Box className={styles.sidebarStyle}>
-            <Stack space={[1, 2]}>
-              <NavigateBack />
-              {sidebar}
-            </Stack>
-          </Box>
+          <Box className={styles.sidebarStyle}>{sidebar}</Box>
           <Box className={styles.mainStyle}>
             <Stack space={[1, 2]}>{children}</Stack>
           </Box>
