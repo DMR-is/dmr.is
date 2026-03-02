5ded153bfad9d49bc78650abcd33673cede9b632
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2026-03-02 15:12:57 +0000

    Return days in html generation in possesive

--- libs/legal-gazette/html/src/lib/constants.ts
+++ libs/legal-gazette/html/src/lib/constants.ts
@@ -8,3 +8,14 @@ export enum LegalGazetteHTMLTemplates {
   DIVISION_ENDING_DECEASED = 'DIVISION_ENDING_DECEASED',
   FORECLOSURE = 'FORECLOSURE',
 }
+
+
+export const ICELANDIC_WEEKDAYS_POSSESIVE_MAP: Record<string, string> = {
+  'sunnudagur': 'sunnudaginn',
+  'mánudagur': 'mánudaginn',
+  'þriðjudagur': 'þriðjudaginn',
+  'miðvikudagur': 'miðvikudaginn',
+  'fimmtudagur': 'fimmtudaginn',
+  'föstudagur': 'föstudaginn',
+  'laugardagur': 'laugardaginn'
+}
