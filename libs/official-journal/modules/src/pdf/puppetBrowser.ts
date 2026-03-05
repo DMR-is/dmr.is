900b0acce44abe6b58f4b61099cf1824946145a4
Þórður H <tolleinn@gmail.com>
2026-03-05 14:50:26 +0000

    fix(ojoi): revert pdf changes (#1177)
    * fix: revert pdf changes
    
    * fix: revert pdf changes

--- libs/official-journal/modules/src/pdf/puppetBrowser.ts
+++ libs/official-journal/modules/src/pdf/puppetBrowser.ts
@@ -1,24 +1,20 @@
-import puppeteer from 'puppeteer'
+import * as puppeteer from 'puppeteer'
 
 export const getBrowser = async () => {
-  const isProduction = process.env.NODE_ENV === 'production'
-
-  const args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--font-render-hinting=none',
-    '--disable-gpu',
-    ...(isProduction ? ['--disable-dev-shm-usage', '--no-zygote'] : []),
-  ]
-
-  const browser = await puppeteer.launch({
-    args,
+  const browers = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--font-render-hinting=none',
+      '--disable-gpu',
+    ],
     headless: true,
     protocolTimeout: 300_000,
-    executablePath: isProduction
-      ? '/usr/bin/chromium-browser'
-      : (process.env.LOCAL_CHROMIUM_PATH ??
-        '/Applications/Chromium.app/Contents/MacOS/Chromium'),
+    executablePath:
+      process.env.NODE_ENV === 'production'
+        ? '/usr/bin/chromium-browser'
+        : (process.env.LOCAL_CHROMIUM_PATH ??
+          '/Applications/Chromium.app/Contents/MacOS/Chromium'),
   })
-  return browser
+  return browers
 }
