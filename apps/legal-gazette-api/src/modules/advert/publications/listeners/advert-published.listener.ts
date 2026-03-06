324b6a152da599b8e80a020a4ce5b8a16c545bf2
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2026-03-06 15:42:33 +0000

    Add more logging

--- apps/legal-gazette-api/src/modules/advert/publications/listeners/advert-published.listener.ts
+++ apps/legal-gazette-api/src/modules/advert/publications/listeners/advert-published.listener.ts
@@ -46,6 +46,8 @@ export class AdvertPublishedListener {
     this.logger.info('Creating TBR transaction for advert', {
       advertId: advert.id,
       context: LOGGING_CONTEXT,
+      advert: advert, // While we debug the publish error
+      publication: publication, // -- || --
     })
 
     const { feeCodeId, paymentData } =
