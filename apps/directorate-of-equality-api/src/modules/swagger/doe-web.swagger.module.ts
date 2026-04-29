ac4d680518506f0b7a3c7ede3defe1d9436fc7e9
Jón Bjarni Ólafsson <dev.jonbjarni@gmail.com>
2026-04-29 11:07:27 +0000

    Udpating the internal swagger module to only import the neccessary controllers required.

--- apps/directorate-of-equality-api/src/modules/swagger/doe-web.swagger.module.ts
+++ apps/directorate-of-equality-api/src/modules/swagger/doe-web.swagger.module.ts
@@ -3,10 +3,6 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '../config/config.module'
 import { ReportModule } from '../report/report.module'
 import { ReportCommentModule } from '../report-comment/report-comment.module'
-import { ReportCreateModule } from '../report-create/report-create.module'
-import { ReportExcelModule } from '../report-excel/report-excel.module'
-import { ReportResultModule } from '../report-result/report-result.module'
-import { ReportStatisticsModule } from '../report-statistics/report-statistics.module'
 import { ReportWorkflowModule } from '../report-workflow/report-workflow.module'
 import { UserModule } from '../user/user.module'
 
@@ -16,10 +12,6 @@ import { UserModule } from '../user/user.module'
     ConfigModule,
     ReportModule,
     ReportCommentModule,
-    ReportCreateModule,
-    ReportExcelModule,
-    ReportResultModule,
-    ReportStatisticsModule,
     ReportWorkflowModule,
   ],
 })
--- apps/directorate-of-equality-web/src/lib/trpc/server/routers/_app.ts
+++ apps/directorate-of-equality-web/src/lib/trpc/server/routers/_app.ts
@@ -1,9 +1,6 @@
 import { router } from '../trpc'
 import { configRouter } from './configRouter'
 import { reportCommentsRouter } from './reportCommentsRouter'
-import { reportCreateRouter } from './reportCreateRouter'
-import { reportResultRouter } from './reportResultRouter'
-import { reportStatisticsRouter } from './reportStatisticsRouter'
 import { reportWorkflowRouter } from './reportWorkflowRouter'
 import { reportsRouter } from './reportsRouter'
 import { userRouter } from './userRouter'
@@ -12,11 +9,8 @@ export const appRouter = router({
   user: userRouter,
   config: configRouter,
   reports: reportsRouter,
-  reportCreate: reportCreateRouter,
   reportComments: reportCommentsRouter,
   reportWorkflow: reportWorkflowRouter,
-  reportResult: reportResultRouter,
-  reportStatistics: reportStatisticsRouter,
 })
 
 export type AppRouter = typeof appRouter
