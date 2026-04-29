adeb75c4dde2a3b87e4ecc43fd241dede5a8d106
Róberta Andersen <robertaandersen1978@gmail.com>
2026-04-29 11:46:34 +0200

    fix(doe): add health check endpoint to directorate-of-equality-api (#1230)
    * fix(doe): add health check endpoint to directorate-of-equality-api
    
    * lint

--- apps/directorate-of-equality-api/src/app/app.module.ts
+++ apps/directorate-of-equality-api/src/app/app.module.ts
@@ -39,6 +39,8 @@ import { ReportStatisticsModule } from '../modules/report-statistics/report-stat
 import { ReportWorkflowModule } from '../modules/report-workflow/report-workflow.module'
 import { UserModel } from '../modules/user/models/user.model'
 import { UserModule } from '../modules/user/user.module'
+import { HealthController } from './health.controller'
+
 @Module({
   imports: [
     LoggingModule,
@@ -92,7 +94,7 @@ import { UserModule } from '../modules/user/user.module'
     ReportModule,
     ReportCreateModule,
   ],
-  controllers: [],
+  controllers: [HealthController],
   providers: [
     {
       provide: APP_FILTER,
