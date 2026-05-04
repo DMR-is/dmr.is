e08bb816ca87ffa68c23d980a450c2c8f3595e57
disaerna <disa@hugsmidjan.is>
2026-05-04 10:47:11 +0000

    fix: tags

--- apps/directorate-of-equality-web/src/components/list-page/Tabs/TabsContainer.tsx
+++ apps/directorate-of-equality-web/src/components/list-page/Tabs/TabsContainer.tsx
@@ -2,10 +2,35 @@
 
 import { GridContainer } from '@dmr.is/ui/components/island-is/GridContainer'
 import { Tabs } from '@dmr.is/ui/components/island-is/Tabs'
+import { Tag, type TagVariant } from '@dmr.is/ui/components/island-is/Tag'
 
 import { MOCK_DATA } from '../../../app/(protected)/mal/mocks'
-import { COLUMN_EMPLOYEES, COLUMN_STATUS } from '../constants'
+import { type Case, COLUMN_EMPLOYEES, COLUMN_STATUS } from '../constants'
 import { TabContent } from './TabContent'
+
+import { type ColumnDef } from '@tanstack/react-table'
+
+const STATUS_VARIANT: Record<string, TagVariant> = {
+  Samþykkt: 'mint',
+  Hafnað: 'red',
+  Lokið: 'dark',
+  'Í vinnslu': 'blue',
+  'Bíður gagna': 'rose',
+  'Til skoðunar': 'purple',
+}
+
+const statusColumn: ColumnDef<Case> = {
+  ...COLUMN_STATUS,
+  cell: ({ getValue }) => {
+    const status = getValue<string>()
+    if (!status) return null
+    return (
+      <Tag variant={STATUS_VARIANT[status] ?? 'blue'} disabled outlined>
+        {status}
+      </Tag>
+    )
+  },
+}
 
 const iVinnsluData = MOCK_DATA.slice(0, 5)
 const afgreittData = MOCK_DATA.slice(0, 3)
@@ -30,7 +55,7 @@ export const TabsContainer = () => {
             content: (
               <TabContent
                 initialData={iVinnsluData}
-                extraColumns={[COLUMN_STATUS, COLUMN_EMPLOYEES]}
+                extraColumns={[statusColumn, COLUMN_EMPLOYEES]}
               />
             ),
           },
