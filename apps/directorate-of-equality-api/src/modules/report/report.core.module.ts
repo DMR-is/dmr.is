cb5d8070a8c899b0845a186fd44d6b5d7a7dda0c
Þórður H <tolleinn@gmail.com>
2026-05-11 12:56:59 +0000

    feat(doe): subsidiary in ReportDetailDto (#1254)
    report-detail-subsidiaries

--- apps/directorate-of-equality-api/src/modules/report/report.core.module.ts
+++ apps/directorate-of-equality-api/src/modules/report/report.core.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 
+import { CompanyReportModel } from '../company/models/company-report.model'
 import { ReportCommentModel } from '../report-comment/models/report-comment.model'
 import { ReportEmployeeOutlierModel } from '../report-employee/models/report-employee-outlier.model'
 import { ReportRoleResultModel } from '../report-result/models/report-role-result.model'
@@ -17,6 +18,7 @@ import { IReportService } from './report.service.interface'
       ReportRoleResultModel,
       ReportEmployeeOutlierModel,
       ReportCommentModel,
+      CompanyReportModel,
     ]),
   ],
   providers: [
