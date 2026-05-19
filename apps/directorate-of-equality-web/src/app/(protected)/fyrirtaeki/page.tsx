9f8b4a50923f1c0d30c3bf8ef98108bf596a9ca1
disaerna <disa@hugsmidjan.is>
2026-05-19 10:53:52 +0000

    fix: remove prefetch

--- apps/directorate-of-equality-web/src/app/(protected)/fyrirtaeki/page.tsx
+++ apps/directorate-of-equality-web/src/app/(protected)/fyrirtaeki/page.tsx
@@ -1,26 +1,12 @@
 import { Suspense } from 'react'
 
-import { getQueryClient } from '@dmr.is/trpc/client/server'
 import { Hero } from '@dmr.is/ui/components/Hero/Hero'
 import { Box } from '@dmr.is/ui/components/island-is/Box'
 import { SearchDashboardLoading } from '@dmr.is/ui/components/SearchDashboard/SearchDashboardLoading'
 
 import { CompaniesContainer } from '../../../containers/companies/CompaniesContainer'
-import { trpc } from '../../../lib/trpc/client/server'
-
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 
 export default async function FyrirtaekiPage() {
-  const queryClient = getQueryClient()
-  await Promise.all([
-    queryClient.prefetchQuery(
-      trpc.company.list.queryOptions({ pageSize: 1000 }),
-    ),
-    queryClient.prefetchQuery(
-      trpc.reports.list.queryOptions({ status: ['APPROVED'], pageSize: 1000 }),
-    ),
-  ])
-
   return (
     <Box height="full">
       <Hero
@@ -36,11 +22,9 @@ export default async function FyrirtaekiPage() {
         withOffset={false}
       />
       <Box background="blue100" paddingY={5} height="full">
-        <HydrationBoundary state={dehydrate(queryClient)}>
-          <Suspense fallback={<SearchDashboardLoading />}>
-            <CompaniesContainer />
-          </Suspense>
-        </HydrationBoundary>
+        <Suspense fallback={<SearchDashboardLoading />}>
+          <CompaniesContainer />
+        </Suspense>
       </Box>
     </Box>
   )
